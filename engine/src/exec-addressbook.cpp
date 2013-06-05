ec3a42803b8094a6152428536b96d961a342f21d
Ali Lloyd <ali.lloyd@runrev.com>
2013-06-05 10:27:16 +0100

    added address book and sensor exec files

---
+++ engine/src/exec-addressbook.cpp
@@ -0,0 +1,118 @@
+/* Copyright (C) 2003-2013 Runtime Revolution Ltd.
+
+This file is part of LiveCode.
+
+LiveCode is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License v3 as published by the Free
+Software Foundation.
+
+LiveCode is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
+
+#include "prefix.h"
+
+#include "globdefs.h"
+#include "filedefs.h"
+#include "objdefs.h"
+#include "parsedef.h"
+#include "mcio.h"
+
+#include "globals.h"
+#include "debug.h"
+#include "handler.h"
+
+#include "mblsyntax.h"
+#include "mblcontact.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+MC_EXEC_DEFINE_EXEC_METHOD(AddressBook, PickContact, 0)
+MC_EXEC_DEFINE_EXEC_METHOD(AddressBook, ShowContact, 1)
+MC_EXEC_DEFINE_EXEC_METHOD(AddressBook, CreateContact, 0)
+MC_EXEC_DEFINE_EXEC_METHOD(AddressBook, UpdateContact, 4)
+MC_EXEC_DEFINE_GET_METHOD(AddressBook, ContactData, 2)
+MC_EXEC_DEFINE_EXEC_METHOD(AddressBook, RemoveContact, 1)
+MC_EXEC_DEFINE_EXEC_METHOD(AddressBook, AddContact, 0)
+MC_EXEC_DEFINE_EXEC_METHOD(AddressBook, FindContact, 4)
+
+////////////////////////////////////////////////////////////////////////////////
+
+void MCAddressBookExecPickContact(MCExecContext& ctxt)
+{
+    int32_t r_result = 0;
+    MCSystemPickContact(r_result);
+    if (r_result > 0)
+        ctxt.SetTheResultToNumber(r_result);
+    else
+        ctxt.SetTheResultToEmpty();
+}
+
+void MCAddressBookExecShowContact(MCExecContext& ctxt, int32_t p_contact_id)
+{
+    int32_t r_result = 0;
+    MCSystemShowContact(p_contact_id, r_result);
+    if (r_result > 0)
+        ctxt.SetTheResultToNumber(r_result);
+    else
+        ctxt.SetTheResultToEmpty();
+}
+
+void MCAddressBookExecCreateContact(MCExecContext& ctxt)
+{
+    int32_t r_result = 0;
+    MCSystemCreateContact(r_result);
+    if (r_result > 0)
+        ctxt.SetTheResultToNumber(r_result);
+    else
+        ctxt.SetTheResultToEmpty();
+}
+
+void MCAddressBookExecUpdateContact(MCExecContext& ctxt, MCArrayRef p_contact, MCStringRef p_title, MCStringRef p_message, MCStringRef p_alternate_name)
+{
+    int32_t r_result = 0;
+    /* UNCHECKED */ MCSystemUpdateContact(p_contact, p_title, p_message, p_alternate_name, r_result);
+    if (r_result > 0)
+        ctxt.SetTheResultToNumber(r_result);
+    else
+        ctxt.SetTheResultToEmpty();
+}
+
+void MCAddressBookGetContactData(MCExecContext& ctxt, int32_t p_contact_id, MCArrayRef& r_contact_data)
+{
+      MCSystemGetContactData(ctxt, p_contact_id, r_contact_data);
+}
+
+void MCAddressBookExecRemoveContact(MCExecContext& ctxt, int32_t p_contact_id)
+{
+    if (MCSystemRemoveContact(p_contact_id))
+		ctxt.SetTheResultToNumber(p_contact_id);
+	else
+        ctxt.SetTheResultToEmpty();
+}
+
+void MCAddressBookExecAddContact(MCExecContext &ctxt, MCArrayRef p_contact)
+{
+	int32_t t_result = 0;
+	/* UNCHECKED */ MCSystemAddContact(p_contact, t_result);
+	if (t_result > 0)
+		ctxt.SetTheResultToNumber(t_result);
+	else
+		ctxt.SetTheResultToEmpty();
+}
+
+void MCAddressBookExecFindContact(MCExecContext& ctxt, MCStringRef p_contact_name)
+{
+	MCAutoStringRef t_result;
+    MCSystemFindContact(p_contact_name, &t_result);
+    if (*t_result != nil)
+        ctxt.SetTheResultToValue(*t_result);
+    else
+        ctxt.SetTheResultToEmpty();
+}
+
+////////////////////////////////////////////////////////////////////////////////
