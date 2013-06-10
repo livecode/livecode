513a7ef92db46f1d0cf513e18491b1b7a1210dfe
Mark Waddingham <mark.waddingham@runrev.com>
2013-06-10 12:04:26 +0100

    [[ Bug 10945 ]] Added referencing comment.

--- engine/src/mblcontrol.cpp
+++ engine/src/mblcontrol.cpp
@@ -377,6 +377,8 @@ void MCNativeControl::Finalize(void)
 
 bool MCNativeControl::List(MCNativeControlListCallback p_callback, void *p_context)
 {
+	// MERG-2013-06-10: [[ Bug 10945 ]] Make sure we only list controls that haven't
+	//   been deleted.
 	for(MCNativeControl *t_control = s_native_controls; t_control != nil; t_control = t_control -> m_next)
 		if (!t_control -> m_deleted)
             if (!p_callback(p_context, t_control))
