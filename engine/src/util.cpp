53c4104c01c37414e26176ce490669e17385bf56
Mark Waddingham <mark.waddingham@runrev.com>
2013-07-10 10:37:42 +0100

    [[ Bug 10964 ]] Ensure MCU_strtol rounds in the same way as getuint4() (and friends).

--- engine/src/util.cpp
+++ engine/src/util.cpp
@@ -442,8 +442,8 @@ int4 MCU_strtol(const char *&sptr, uint4 &l, int1 c, Boolean &done,
 						{
 							if (reals)
 							{
-//								MDW 2013-06-09 : allow rounding
-//								see MCU_parsepoint and MCU_parsepoints
+								// MDW-2013-06-09: [[ Bug 10964 ]] Round integral values to nearest
+								//   (consistent with getuint4() and round()).
 								if (*(sptr+1) > '4')
 								{
 									value++;
@@ -1311,9 +1311,8 @@ Boolean MCU_parsepoints(MCPoint *&points, uint2 &noldpoints,
 	while (l)
 	{
 		Boolean done1, done2;
-		// MDW 2013-06-09 : allow non-integer points to be set, round to ints
-		int2 i1= (int2)MCU_strtol(sptr, l, ',', done1, True);
-		int2 i2 = (int2)MCU_strtol(sptr, l, ',', done2, True);
+		int2 i1= MCU_strtol(sptr, l, ',', done1);
+		int2 i2 = MCU_strtol(sptr, l, ',', done2);
 		while (l && !isdigit((uint1)*sptr) && *sptr != '-' && *sptr != '+')
 		{
 			l--;
@@ -1347,9 +1346,8 @@ Boolean MCU_parsepoint(MCPoint &point, const MCString &data)
 	const char *sptr = data.getstring();
 	uint4 l = data.getlength();
 	Boolean done1, done2;
-	// MDW 2013-06-09 : allow non-integer points to be set, round to ints
-	int2 i1= (int2)(MCU_strtol(sptr, l, ',', done1, True));
-	int2 i2 = (int2)(MCU_strtol(sptr, l, ',', done2, True));
+	int2 i1= MCU_strtol(sptr, l, ',', done1);
+	int2 i2 = MCU_strtol(sptr, l, ',', done2);
 	if (!done1 || !done2)
 	{
 		i1 = i2 = MININT2;
