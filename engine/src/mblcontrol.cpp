f7cc7d5ffa46a5768cacbaf174a499e94d0e354d
Panos Merakos <panos.merakos@runrev.com>
2013-09-19 16:19:50 +0100

    Updates after Seb's feedback #2

--- engine/src/mblcontrol.cpp
+++ engine/src/mblcontrol.cpp
@@ -368,8 +368,8 @@ bool MCNativeControl::FindByNameOrId(MCStringRef p_name, MCNativeControl*& r_con
 	//uint32_t t_id;
 	MCNumberRef t_id;
 	//if (MCStringToInteger(p_name, t_id))
-	if (MCNumberParse(p_name, t_id)
-		return FindById(t_id, r_control);
+	if (MCNumberParse(p_name, t_id))
+		return FindById(MCNumberFetchAsUnsignedInteger(t_id), r_control);
 	
 	for(MCNativeControl *t_control = s_native_controls; t_control != nil; t_control = t_control -> m_next)
 		if (!t_control -> m_deleted && t_control -> GetName() != nil && MCStringIsEqualToCString(p_name, t_control -> GetName(), kMCCompareCaseless))
