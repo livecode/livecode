4bcf3d4b5c244e2349d4b4e2e5348a31ce752adc
Fraser J. Gordon <fraser.gordon@runrev.com>
2013-09-23 16:23:02 +0100

    The server Include command now uses an ExecContext

--- engine/src/srvmain.cpp
+++ engine/src/srvmain.cpp
@@ -30,6 +30,7 @@ along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
 #include "dispatch.h"
 #include "mcerror.h"
 #include "execpt.h"
+#include "exec.h"
 #include "object.h"
 #include "hndlrlst.h"
 #include "handler.h"
@@ -527,19 +528,19 @@ void X_main_loop(void)
 #endif
 	
 	MCExecPoint ep;
-	if (!MCserverscript -> Include(ep, MCserverinitialscript, false) &&
+	MCExecContext ctxt(ep);
+	if (!MCserverscript -> Include(ctxt, MCserverinitialscript, false) &&
 		MCS_get_errormode() != kMCSErrorModeDebugger)
 	{
-		char *t_eerror, *t_efiles;
-		t_eerror = MCeerror -> getsvalue() . clone();
-		MCserverscript -> ListFiles(ep);
-		t_efiles = ep . getsvalue() . clone();
+		MCAutoStringRef t_eerror, t_efiles;
+		/* UNCHECKED */ MCeerror->copyasstringref(&t_eerror);
+		MCserverscript -> ListFiles(&t_efiles);
 		MCeerror -> clear();
 		
 		MCParameter t_exec_stack, t_files;
-		t_exec_stack . sets_argument(t_eerror);
+		t_exec_stack . setvalueref_argument(*t_eerror);
 		t_exec_stack . setnext(&t_files);
-		t_files . sets_argument(t_efiles);
+		t_files . setvalueref_argument(*t_efiles);
 		
 		Exec_stat t_stat;
 		t_stat = MCserverscript -> message(MCM_script_execution_error, &t_exec_stack);
@@ -563,12 +564,9 @@ void X_main_loop(void)
 		
 		if ((t_stat != ES_NORMAL && t_stat != ES_PASS) && MCS_get_errormode() != kMCSErrorModeQuiet)
 		{
-			IO_printf(IO_stderr, "ERROR:\n%s\n", t_eerror);
-			IO_printf(IO_stderr, "FILES:\n%s\n", t_efiles);
+			IO_printf(IO_stderr, "ERROR:\n%@\n", *t_eerror);
+			IO_printf(IO_stderr, "FILES:\n%@\n", *t_efiles);
 		}
-
-		delete t_eerror;
-		delete t_efiles;
 	}
 	
 	if (s_server_cgi)
