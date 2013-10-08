dd17f7b3bc8edb85057133aafab12efd0fc1d6cb
Fraser J. Gordon <fraser.gordon@runrev.com>
2013-10-08 11:11:03 +0100

    [[ Bugfix 11261 ]] Over-zealous fix for 11162 broke parsing of internet dates

--- engine/src/date.cpp
+++ engine/src/date.cpp
@@ -445,19 +445,11 @@ static bool datetime_parse(const MCDateTimeLocale *p_locale, int4 p_century_cuto
 				t_valid = true;
 			break;
 			}
-			
-			// FG-2013-09-10 [[ Bug 11162 ]]
-			// Trailing spaces after an accepted format specifier should be squashed
-			if (t_valid && !t_skip)
-			{
-				while (p_format[1] != '\0' && isspace(p_format[1]))
-					p_format++;
-				while (t_input_length > 1 && isspace(t_input[1]))
-					t_input++, t_input_length--;
-			}
 
 		}
-		else if (*p_format != *t_input)
+		// FG-2013-10-08 [[ Bugfix 11261 ]]
+		// If there is an extra space in the format string, treat it as a match
+		else if (*p_format != *t_input && !isspace(*p_format))
 		{
 			// Unrecognised padding is optional, so advance and skip
 			if (t_loose_separators)
@@ -486,15 +478,25 @@ static bool datetime_parse(const MCDateTimeLocale *p_locale, int4 p_century_cuto
 		{
 			// FG-2013-09-10 [[ Bug 11162 ]]
 			// One or more spaces in the format string should accept any number of input spaces
-			while (t_input_length > 0 && isspace(*t_input))
-				t_input += 1, t_input_length -= 1;
-			while (*p_format != '\0' && isspace(*p_format))
-				p_format++;
-			
-			if (t_input_length > 0)
+			// FG-2013-10-08 [[ Bug 11261 ]]
+			// Over-incrementing of format pointer caused internet dates to parse incorrectly
+			if (isspace(*p_format))
 			{
-				t_input += 1;
-				t_input_length -= 1;
+				while (t_input_length > 0 && isspace(*t_input))
+					t_input += 1, t_input_length -= 1;
+				
+				// Format is incremented past the current char below so just
+				// remove additional spaces here and leave one for it.
+				while (p_format[1] != '\0' && isspace(p_format[1]))
+					p_format++;
+			}
+			else
+			{
+				if (t_input_length > 0)
+				{
+					t_input += 1;
+					t_input_length -= 1;
+				}
 			}
 			t_valid = true;
 		}
