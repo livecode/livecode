a4ff1e8ffc0c8fef9817dc3c749fb8d0638c0cce
runrevsebastien <sebastien@runrev.com>
2013-10-14 12:31:38 +0100

    Update MCVariable::copyasvalueref to return false on memory failure only

--- engine/src/variable.cpp
+++ engine/src/variable.cpp
@@ -237,19 +237,12 @@ MCValueRef MCVariable::getvalueref(MCNameRef *p_path, uindex_t p_length, bool p_
 
 bool MCVariable::copyasvalueref(MCValueRef &r_value)
 {
-    return copyasvalueref(nil, 0, false, r_value);
+    return MCValueCopy(getvalueref(nil, 0, false), r_value);
 }
 
 bool MCVariable::copyasvalueref(MCNameRef *p_path, uindex_t p_length, bool p_case_sensitive, MCValueRef &r_value)
 {
-    MCValueRef t_value;
-    
-    t_value = getvalueref(p_path, p_length, p_case_sensitive);
-    
-    if (t_value != kMCEmptyString)
-        return MCValueCopy(t_value, r_value);
-    
-    return false;
+    return MCValueCopy(getvalueref(p_path, p_length, p_case_sensitive));
 }
 
 Exec_stat MCVariable::eval(MCExecPoint& ep)
