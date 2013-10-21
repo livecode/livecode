f3d0eab8b03657d5bdeca942a38df04aea3c087f
Panos Merakos <panos.merakos@runrev.com>
2013-10-21 11:03:04 +0100

    Updates on methods: can_access_file() can_access_host() can_access_library() decode_struct()

--- engine/src/externalv0.cpp
+++ engine/src/externalv0.cpp
@@ -78,7 +78,7 @@ struct MCarray
 };
 
 typedef char *(*XCB)(const char *arg1, const char *arg2, const char *arg3, int *retval);
-typedef int (*SECURITYHANDLER)(const char *);
+typedef int (*SECURITYHANDLER)(MCStringRef);
 typedef void (*DELETER)(void *data);
 typedef void (*GETXTABLE)(XCB *, DELETER, const char **, Xternal **, DELETER *);
 typedef void (*CONFIGURESECURITY)(SECURITYHANDLER *handlers);
@@ -784,21 +784,19 @@ XCB MCcbs[] =
 
 ////////////////////////////////////////////////////////////////////////////////
 
-static int can_access_file(const char *p_file)
+static int can_access_file(MCStringRef p_file)
 {
 	return MCSecureModeCanAccessDisk();
 }
 
-static int can_access_host(const char *p_host)
+static int can_access_host(MCStringRef p_host)
 {
 	if (MCSecureModeCanAccessNetwork())
 		return true;
-    MCAutoStringRef t_host;
-    /* UNCHECKED */ MCStringCreateWithCString(p_host, &t_host);
-	return MCModeCanAccessDomain(*t_host);
+	return MCModeCanAccessDomain(p_host);
 }
 
-static int can_access_library(const char *p_host)
+static int can_access_library(MCStringRef p_host)
 {
 	return true;
 }
