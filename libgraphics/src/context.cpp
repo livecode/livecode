c68d11b27cea92cf31a7cdcd559faf4fc8042bce
Michael McCreary <michael.mccreary@runrev.com>
2013-11-08 16:12:59 +0000

    [[ Bugfix 11388 ]] 1 pixel non-antialiased lines draw incorrectly in certain circumstances.

--- libgraphics/src/context.cpp
+++ libgraphics/src/context.cpp
@@ -2217,11 +2217,15 @@ static bool MCGContextSetupStrokePaint(MCGContextRef self, SkPaint &r_paint)
 	{				
 		r_paint . setStyle(SkPaint::kStroke_Style);
 		r_paint . setAntiAlias(self -> state -> should_antialias);
-		r_paint . setStrokeWidth(MCGFloatToSkScalar(self -> state -> stroke_attr . width));
 		r_paint . setStrokeMiter(MCGFloatToSkScalar(self -> state -> stroke_attr . miter_limit));
 		r_paint . setStrokeJoin(MCGJoinStyleToSkJoinStyle(self -> state -> stroke_attr . join_style));
 		r_paint . setStrokeCap(MCGCapStyleToSkCapStyle(self -> state -> stroke_attr . cap_style));	
 		r_paint . setPathEffect(t_dash_effect);
+		
+		// MM-2013-11-08: [[ Bug 11388 ]] For 1 pixel lines, don't set the line width. Appeared to be causing issues with certain non-antialiased lines.
+		if (self -> state -> stroke_attr . width != 1.0f)
+			r_paint . setStrokeWidth(MCGFloatToSkScalar(self -> state -> stroke_attr . width));
+
 		
 		SkXfermode *t_blend_mode;
 		t_blend_mode = MCGBlendModeToSkXfermode(self -> state -> blend_mode);
