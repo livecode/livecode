94b4c7665c446b06fc301376f0832e321352fdac
runrevpanos <panos.merakos@runrev.com>
2013-11-11 10:14:56 +0000

    Updates after Seb's feedback

--- engine/src/osxansold.cpp
+++ engine/src/osxansold.cpp
@@ -670,6 +670,8 @@ int MCA_file_tiger(MCStringRef p_title, MCStringRef p_prompt, MCStringRef p_filt
         
         t_filters = new FilterRecord[1];
 		t_filters[0] . tag = MCValueRetain(kMCEmptyString);
+        /* UNCHECKED */ MCArrayMutableCopyAndRelease(t_filters[0] . file_types, t_filters[0] . file_types);
+        t_filters[0] . file_types = nil;
         
         for(unsigned int t_index = 0; t_index < t_filetype_count; ++t_index)
         {
@@ -709,6 +711,8 @@ int MCA_ask_file_tiger(MCStringRef p_title, MCStringRef p_prompt, MCStringRef p_
         
         t_filters = new FilterRecord[1];
 		t_filters[0] . tag = MCValueRetain(kMCEmptyString);
+        /* UNCHECKED */ MCArrayMutableCopyAndRelease(t_filters[0] . file_types, t_filters[0] . file_types);
+        t_filters[0] . file_types = nil;
         
         for(unsigned int t_index = 0; t_index < t_filetype_count; ++t_index)
         {
