a9d1ccad616ba8e84dc024e1b893bf2add31e56e
Mark Waddingham <mark.waddingham@runrev.com>
2013-11-22 14:49:47 +0000

    [[ RefactorSyntax ]] Fixed file dialog opening and filtering on Mac.

--- engine/src/osxans.mm
+++ engine/src/osxans.mm
@@ -545,11 +545,7 @@ static int display_modal_dialog(NSSavePanel *p_panel, MCStringRef p_initial_fold
 	}
 	else
 		// MM-2012-03-16: [[ Bug ]] Use runModalForDirectory:file: rather than setting directory and file - methods only introduced in 10.6
-//		return [p_panel runModalForDirectory: t_initial_folder file: t_initial_file];
-    {
-        [p_panel setDirectoryURL: [NSURL URLWithString: t_initial_folder]];
-        return [p_panel runModal];
-    }
+		return [p_panel runModalForDirectory: t_initial_folder file: t_initial_file];
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -620,7 +616,8 @@ int MCA_do_file_dialog(MCStringRef p_title, MCStringRef p_prompt, MCStringRef *p
 		}
 		else
 			[t_panel setTitle: [NSString stringWithMCStringRef: p_prompt]];
-		
+		[t_panel setDelegate: t_accessory];
+
 		if (p_type_count > 1)
 		{
 			[t_accessory setLabel: @"Format:"];
