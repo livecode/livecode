8430076f4c28aee409d3baf1a521511d9e78be98
Fraser J. Gordon <fraser.gordon@runrev.com>
2013-12-11 10:01:03 +0000

    Fix I/O for non-blocking file streams on OSX

--- engine/src/dskmac.cpp
+++ engine/src/dskmac.cpp
@@ -2101,17 +2101,19 @@ public:
             if (ferror(m_stream))
             {
                 clearerr(m_stream);
+                m_is_eof = false;
                 
                 if (errno == EAGAIN)
-                    return false;
+                    return true;
                 
                 if (errno == EINTR)
                 {
                     toread -= nread;
                     continue;
                 }
-                else
-                    return false;
+                
+                // A "real" error occurred
+                return false;
             }
             if (feof(m_stream))
             {
