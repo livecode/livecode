ab761854e2afca6b7b6934deff6ddf937bf4b6a0
runrevali <ali@runrev.com>
2014-01-16 12:00:00 +0000

    [[ Bug 11677 ]] Removed minimum width/height ratio check in linux theme drawing.

--- engine/src/lnxgtktheme.cpp
+++ engine/src/lnxgtktheme.cpp
@@ -467,7 +467,9 @@ void MCNativeTheme::getscrollbarrects(const MCWidgetInfo & winfo,
 		real8 range = sbinfo->endvalue - sbinfo->startvalue;
 		if (winfo.attributes & WTHEME_ATT_SBVERTICAL)
 		{
-			if ( (sbinfo->thumbsize != 0 ) && srect.height > srect.width * 3)
+            // AL-2014-01-16: [[ Bug 11677 ]] No need to make thumb disappear at specific width/height ratio
+			// if ( (sbinfo->thumbsize != 0 ) && srect.height > srect.width * 3)
+            if ( (sbinfo->thumbsize != 0 ) )
 			{
 				sbthumbrect.x = srect.x + troughBorder;
 				sbthumbrect.width = trackwidth;
@@ -552,7 +554,9 @@ void MCNativeTheme::getscrollbarrects(const MCWidgetInfo & winfo,
 		}
 		else
 		{
-			if ( (sbinfo->thumbsize != 0 ) && srect.width > srect.height * 3)
+            // AL-2014-01-16: [[ Bug 11677 ]] No need to make thumb disappear at specific width/height ratio
+			//if ( (sbinfo->thumbsize != 0 ) && srect.width > srect.height * 3)
+            if ( (sbinfo->thumbsize != 0 ) )
 			{
 				sbthumbrect.y = srect.y + troughBorder;
 				sbthumbrect.height = trackheight;
