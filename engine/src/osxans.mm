acc7f5360c37b168536efbad0f653175ed7de437
Michael McCreary <michael.mccreary@runrev.com>
2014-01-17 15:33:47 +0000

    [[ Bugfix 11402 ]] Attempt to update thirdparty submodule reference correctly. [[ Bugfix 11402 ]] Tweaks to get things to compile on OSX.

--- engine/src/osxans.mm
+++ engine/src/osxans.mm
@@ -276,12 +276,14 @@ static bool filter_to_type_list(const char *p_filter, char ***r_types, uint32_t 
 		/* UNCHECKED */ MCCStringFree(t_types);
 	}		
 	
-	return t_success;	
+	return t_success;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-@interface FileDialogAccessoryView : NSView
+// MM-2014-01-09: [[ Bug 11402 ]] The update to libskia required a compiler update.
+//  Update interface definition in order to appease the new cmpiler.
+@interface FileDialogAccessoryView : NSView <NSOpenSavePanelDelegate>
 {
 	NSTextField *m_label;
 	NSPopUpButton *m_options;
