809e1a10215264f0f4304d4492e6ad5e617cbeed
runrevsebastien <sebastien@runrev.com>
2014-03-12 12:06:27 +0000

    [[ RefactorSyntax ]] Fixes over-release on any math operation applied to an array

--- engine/src/exec.cpp
+++ engine/src/exec.cpp
@@ -256,12 +256,12 @@ bool MCExecContext::ConvertToNumberOrArray(MCExecValue& x_value)
         double t_real;
         if (!ConvertToReal(x_value . valueref_value, t_real))
         {
-            MCAutoArrayRef t_array;
-            if (!ConvertToArray(x_value . valueref_value, &t_array))
+            MCArrayRef t_array;
+            if (!ConvertToArray(x_value . valueref_value, t_array))
                 return false;
             
             MCValueRelease(x_value . valueref_value);
-            MCExecValueTraits<MCArrayRef>::set(x_value, *t_array);
+            MCExecValueTraits<MCArrayRef>::set(x_value, t_array);
             return true;
         }
 
