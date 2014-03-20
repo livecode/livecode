5f4dd03f8d1c620a3aee8d519e5f975a76277284
Mark Waddingham <mark.waddingham@runrev.com>
2014-03-20 10:50:07 +0000

    [[ Cocoa ]] Make sure QuickTime player support is included on Windows.

--- engine/src/sysdefs.h
+++ engine/src/sysdefs.h
@@ -39,6 +39,7 @@ along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
 #define MCSSL
 #define FEATURE_TASKBAR_ICON
 #define FEATURE_RELAUNCH_SUPPORT
+#define FEATURE_QUICKTIME
 #define FEATURE_QUICKTIME_EFFECTS
 
 #elif defined(_MAC_DESKTOP)
