864d72b1f00022027651024d4c032971af28ff44
runrevpanos <panos.merakos@runrev.com>
2014-03-20 13:55:18 +0000

    Removed the Amazon stuff from the Community edition and updated the android build script

--- engine/src/java/com/runrev/android/billing/BillingModule.java
+++ engine/src/java/com/runrev/android/billing/BillingModule.java
@@ -17,14 +17,12 @@
 package com.runrev.android.billing;
 
 import com.runrev.android.Engine;
-import com.runrev.android.billing.amazon.*;
-import com.runrev.android.billing.google.*;
-import com.runrev.android.billing.samsung.*;
 
 import android.app.*;
 import android.util.*;
 import android.content.*;
 
+import java.lang.reflect.*;
 import java.util.*;
 
 public class BillingModule
@@ -37,14 +35,29 @@ public class BillingModule
         String t_billing_provider = Engine.doGetCustomPropertyValue("cREVStandaloneSettings", "android,billingProvider");
         Log.d(TAG, "Provider is " + t_billing_provider);
         if (t_billing_provider.equals("Google"))
-            return new GoogleBillingProvider();
+            return loadBillingProvider("com.runrev.android.billing.google.GoogleBillingProvider");
         else if (t_billing_provider.equals("Amazon"))
-            return new AmazonBillingProvider();
+            return loadBillingProvider("com.runrev.android.billing.amazon.AmazonBillingProvider");
         else if (t_billing_provider.equals("Samsung"))
-            return new SamsungBillingProvider();
+            return loadBillingProvider("com.runrev.android.billing.samsung.SamsungBillingProvider");
         else
             return null;
-        
+    }
+    
+    private BillingProvider loadBillingProvider(String p_provider)
+    {
+        try
+        {
+            Class t_billing_provider;
+            Constructor t_billing_provider_constructor;
+            t_billing_provider = Class.forName(p_provider);
+            t_billing_provider_constructor = t_billing_provider.getConstructor(new Class[] {});
+            return (BillingProvider) t_billing_provider_constructor.newInstance(new Object[] {});
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
     }
     
 }
