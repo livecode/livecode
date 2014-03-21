5417d882b16ef20de0d1edad6adaa89de020d9b3
Mark Waddingham <mark.waddingham@runrev.com>
2014-03-21 12:28:57 +0000

    [[ Faster ]] Fixed crash on exit due to over-freeing pools.

--- libfoundation/src/foundation-value.cpp
+++ libfoundation/src/foundation-value.cpp
@@ -807,6 +807,7 @@ void __MCValueFinalize(void)
             __MCValue *t_value;
             t_value = s_value_pools[i] . values;
             s_value_pools[i] . values = *(__MCValue **)t_value;
+			s_value_pools[i] . count -= 1;
             MCMemoryDelete(t_value);
         }
     
