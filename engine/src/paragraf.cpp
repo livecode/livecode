8326216e04d1469c18047b3c4333d5bfd70c11ce
Fraser J. Gordon <fraser.gordon@runrev.com>
2014-04-01 13:27:22 +0100

    Fix selecting words with combining chars in fields by double-clicking

--- engine/src/paragraf.cpp
+++ engine/src/paragraf.cpp
@@ -4633,50 +4633,32 @@ static codepoint_t GetCodepointAtRelativeIndex(MCBlock *p_block, findex_t p_inde
 
 findex_t MCParagraph::findwordbreakbefore(MCBlock *p_block, findex_t p_index)
 {
-	codepoint_t xc, x, y, yc;
-	xc = GetCodepointAtRelativeIndex(p_block, p_index, -2);
-	x = GetCodepointAtRelativeIndex(p_block, p_index, -1);
-	y = GetCodepointAtRelativeIndex(p_block, p_index, 0);
-	yc = GetCodepointAtRelativeIndex(p_block, p_index, 1);
-
-	while(p_block != NULL)
-	{
-		if (MCUnicodeCanBreakWordBetween(xc, x, y, yc))
-			return p_index;
-
-		p_block = p_block -> RetreatIndex(p_index);
-
-		yc = y;
-		y = x;
-		x = xc;
-		xc = GetCodepointAtRelativeIndex(p_block, p_index, -2);
-	}
-
-	return p_index;
+	// Create the word break iterator
+    MCBreakIteratorRef t_breaker;
+    MCLocaleBreakIteratorCreate(kMCBasicLocale, kMCBreakIteratorTypeWord, t_breaker);
+    MCLocaleBreakIteratorSetText(t_breaker, m_text);
+    
+    // Find the preceding word break
+    findex_t t_break;
+    t_break = MCLocaleBreakIteratorBefore(t_breaker, p_index);
+    MCLocaleBreakIteratorRelease(t_breaker);
+    
+    return (t_break == kMCLocaleBreakIteratorDone) ? MCStringGetLength(m_text) : t_break;
 }
 
 findex_t MCParagraph::findwordbreakafter(MCBlock *p_block, findex_t p_index)
 {
-	uint4 xc, x, y, yc;
-	xc = GetCodepointAtRelativeIndex(p_block, p_index, -1);
-	x = GetCodepointAtRelativeIndex(p_block, p_index, 0);
-	y = GetCodepointAtRelativeIndex(p_block, p_index, 1);
-	yc = GetCodepointAtRelativeIndex(p_block, p_index, 2);
-
-	while(p_block != NULL)
-	{
-		if (MCUnicodeCanBreakWordBetween(xc, x, y, yc))
-			return p_index;
-
-		p_block = p_block -> AdvanceIndex(p_index);
-
-		xc = x;
-		x = y;
-		y = yc;
-		yc = GetCodepointAtRelativeIndex(p_block, p_index, 2);
-	}
-
-	return p_index;
+	// Create the word break iterator
+    MCBreakIteratorRef t_breaker;
+    MCLocaleBreakIteratorCreate(kMCBasicLocale, kMCBreakIteratorTypeWord, t_breaker);
+    MCLocaleBreakIteratorSetText(t_breaker, m_text);
+    
+    // Find the succeeding word break
+    findex_t t_break;
+    t_break = MCLocaleBreakIteratorAfter(t_breaker, p_index);
+    MCLocaleBreakIteratorRelease(t_breaker);
+    
+    return (t_break == kMCLocaleBreakIteratorDone) ? MCStringGetLength(m_text) : t_break;
 }
 
 void MCParagraph::sethilite(Boolean newstate)
