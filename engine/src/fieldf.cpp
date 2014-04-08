b55e507432d0204c30ede4d62ab2685e9705ae1e
runrevpanos <panos.merakos@runrev.com>
2014-04-08 15:07:20 +0100

    Updates after Mark's feedback

--- engine/src/fieldf.cpp
+++ engine/src/fieldf.cpp
@@ -1062,15 +1062,11 @@ void MCField::drawrect(MCDC *dc, const MCRectangle &dirty)
 				
 				// MW-2012-03-19: [[ FixedTable ]] If we have reached the final tab in fixed
 				//   table mode, we are done.
-				if (ct == nt - 2 && t[nt - 2] == t[nt - 1])
-					break;
-                
-                // PM-2014-04-08: [[ Bug 12146 ]] Setting tabstops to 2 equal numbers and then
+				// PM-2014-04-08: [[ Bug 12146 ]] Setting tabstops to 2 equal numbers and then
                 //  turning VGrid on, hangs LC, because this while loop ran forever
-                if (ct == nt - 1 && t[nt - 2] == t[nt - 1])
+                if (ct == nt - 1 && (nt < 2 || t[nt - 2] == t[nt - 1]))
                     break;
 			}
-            
 		}
 
 		if (cursoron && cursorfield == this)
