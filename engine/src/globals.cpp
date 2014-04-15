a35dcaab82a9cdfa78154e04dad9888fb1dbc744
Mark Waddingham <mark.waddingham@runrev.com>
2014-04-15 14:45:28 +0100

    [[ Bug 12185 ]] Make sure graphics and openssl are initialized before anything that might need them.

--- engine/src/globals.cpp
+++ engine/src/globals.cpp
@@ -844,7 +844,18 @@ bool X_open(int argc, char *argv[], char *envp[])
 	MCU_init();
 	
 	////
-
+    
+    // MW-2014-04-15: [[ Bug 12185 ]] Initialize graphics and openssl before anything
+    //   that might use them.
+    
+	// MM-2013-09-03: [[ RefactorGraphics ]] Initialize graphics library.
+	MCGraphicsInitialize();
+	
+	// MM-2014-02-14: [[ LibOpenSSL 1.0.1e ]] Initialise the openlSSL module.
+	InitialiseSSL();
+    
+    ////
+    
 	MCpatternlist = new MCImageList();
 
 	/* UNCHECKED */ MCVariable::ensureglobal(MCN_msg, MCmb);
@@ -985,12 +996,6 @@ bool X_open(int argc, char *argv[], char *envp[])
 
 	MCsystemprinter = MCprinter = MCscreen -> createprinter();
 	MCprinter -> Initialize();
-
-	// MM-2013-09-03: [[ RefactorGraphics ]] Initialize graphics library.
-	MCGraphicsInitialize();
-	
-	// MM-2014-02-14: [[ LibOpenSSL 1.0.1e ]] Initialise the openlSSL module.
-	InitialiseSSL();
 	
 	// MW-2009-07-02: Clear the result as a startup failure will be indicated
 	//   there.
