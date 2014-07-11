c5e3970eda0d2e9f1d99efccca9d27e5782242b5
runrevsebastien <sebastien@runrev.com>
2014-07-11 10:29:55 +0100

    [[ Bug 12769 ]] setting dragData["files"] does not work

--- engine/src/mbldc.cpp
+++ engine/src/mbldc.cpp
@@ -899,7 +899,8 @@ MCPasteboard *MCScreenDC::getclipboard(void)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-MCDragAction MCScreenDC::dodragdrop(MCPasteboard *p_pasteboard, MCDragActionSet p_allowed_actions, MCImage *p_image, const MCPoint *p_image_offset)
+// SN-2014-07-11: [[ Bug 12769 ]] Update the signature - the non-implemented UIDC dodragdrop was called otherwise
+MCDragAction MCScreenDC::dodragdrop(Window w, MCPasteboard *p_pasteboard, MCDragActionSet p_allowed_actions, MCImage *p_image, const MCPoint* p_image_offset)
 {
 	return DRAG_ACTION_NONE;
 }
--- engine/src/w32dnd.cpp
+++ engine/src/w32dnd.cpp
@@ -100,7 +100,8 @@ HRESULT DropSource::GiveFeedback(DWORD p_effect)
 	return DRAGDROP_S_USEDEFAULTCURSORS;
 }
 
-MCDragAction MCScreenDC::dodragdrop(MCPasteboard *p_pasteboard, MCDragActionSet p_allowed_actions, MCImage *p_image, const MCPoint *p_image_offset)
+// SN-2014-07-11: [[ Bug 12769 ]] Update the signature - the non-implemented UIDC dodragdrop was called otherwise
+MCDragAction MCScreenDC::dodragdrop(Window w, MCPasteboard *p_pasteboard, MCDragActionSet p_allowed_actions, MCImage *p_image, const MCPoint* p_image_offset)
 {
 	bool t_success;
 	t_success = true;
