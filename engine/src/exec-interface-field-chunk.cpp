fa7f21ccaed095d261f723afa284b5034705bc64
runrevali <ali@runrev.com>
2014-07-14 10:16:16 +0100

    [[ Bug 12789 ]] Relayout after property setting when defragging causes block changes

--- engine/src/exec-interface-field-chunk.cpp
+++ engine/src/exec-interface-field-chunk.cpp
@@ -1031,7 +1031,9 @@ template<typename T> void SetCharPropOfCharChunk(MCExecContext& ctxt, MCField *p
             }
             // end of MCParagraph scope
 
-            if (t_need_layout && !all && pgptr->getopened())
+            // AL-2014-07-14: [[ Bug 12789 ]] Defragging can cause paragraph to need layout, do make sure we relayout
+            //  if it did. Otherwise setting properties that avoid relayout can cause crashes.
+            if (pgptr -> getneedslayout() && !all && pgptr->getopened())
             {
                 // MW-2012-01-25: [[ ParaStyles ]] Ask the paragraph to reflow itself.
                 pgptr -> layout(false);
