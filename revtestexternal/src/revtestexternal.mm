42dfa8b8688602be074c6294c4b5b8cb771aadca
Mark Waddingham <mark.waddingham@runrev.com>
2014-07-16 14:43:04 +0100

    Added revtestexternal.mm file.

---
+++ revtestexternal/src/revtestexternal.mm
@@ -0,0 +1,36 @@
+/* Copyright (C) 2003-2013 Runtime Revolution Ltd.
+ 
+ This file is part of LiveCode.
+ 
+ LiveCode is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License v3 as published by the Free
+ Software Foundation.
+ 
+ LiveCode is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
+
+#include <cstdio>
+#include <cstdarg>
+
+#import <Foundation/Foundation.h>
+
+#include <LiveCode.h>
+
+////////////////////////////////////////////////////////////////////////////////
+
+NSArray *revTestExternalTestObjcArrays(NSArray *array)
+{
+    return array;
+}
+
+NSDictionary *revTestExternalTestObjcDictionaries(NSDictionary *dictionary)
+{
+    return dictionary;
+}
+
+////////////////////////////////////////////////////////////////////////////////
