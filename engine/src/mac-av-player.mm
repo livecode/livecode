dc0d28318d7908caed4819b0845a986ef581bc9d
Mark Waddingham <mark.waddingham@runrev.com>
2014-08-06 14:58:26 +0100

    [[ Bug ]] Make sure we don't wait on a player that hasn't been created.

--- engine/src/mac-av-player.mm
+++ engine/src/mac-av-player.mm
@@ -797,13 +797,16 @@ void MCAVFoundationPlayer::SetProperty(MCPlatformPlayerProperty p_property, MCPl
             // MW-2014-07-29: [[ Bug 12989 ]] Make sure we use the duration timescale.
             // MW-2014-08-01: [[ Bug 13046 ]] Use a completion handler to wait until the currentTime is
             //   where we want it to be.
-            __block bool t_is_finished = false;
-            [[m_player currentItem] seekToTime:CMTimeFromLCTime(*(uint32_t *)p_value) toleranceBefore:kCMTimeZero toleranceAfter:kCMTimeZero completionHandler:^(BOOL finished) {
-                t_is_finished = true;
-                MCPlatformBreakWait();
-            }];
-            while(!t_is_finished)
-                MCPlatformWaitForEvent(60.0, true);
+            if (m_player != nil)
+            {
+                __block bool t_is_finished = false;
+                [[m_player currentItem] seekToTime:CMTimeFromLCTime(*(uint32_t *)p_value) toleranceBefore:kCMTimeZero toleranceAfter:kCMTimeZero completionHandler:^(BOOL finished) {
+                    t_is_finished = true;
+                    MCPlatformBreakWait();
+                }];
+                while(!t_is_finished)
+                    MCPlatformWaitForEvent(60.0, true);
+            }
         }
         break;
 		case kMCPlatformPlayerPropertyStartTime:
@@ -832,10 +835,12 @@ void MCAVFoundationPlayer::SetProperty(MCPlatformPlayerProperty p_property, MCPl
         }
             break;
 		case kMCPlatformPlayerPropertyPlayRate:
-            [m_player setRate: *(double *)p_value];
+            if (m_player != nil)
+                [m_player setRate: *(double *)p_value];
 			break;
 		case kMCPlatformPlayerPropertyVolume:
-            [m_player setVolume: *(uint16_t *)p_value / 100.0f];
+            if (m_player != nil)
+                [m_player setVolume: *(uint16_t *)p_value / 100.0f];
 			break;
         case kMCPlatformPlayerPropertyOnlyPlaySelection:
 			m_play_selection_only = *(bool *)p_value;
