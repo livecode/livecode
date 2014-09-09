da19ced7c77f9ff524a9a2e98b94afbd5ec11f22
Fraser J. Gordon <fraser.gordon@runrev.com>
2014-09-09 16:07:31 +0100

    [[ Bugfix 13259 ]] Fix system time formatting on Windows

--- engine/src/w32date.cpp
+++ engine/src/w32date.cpp
@@ -277,11 +277,14 @@ static MCStringRef windows_convert_time_format(MCStringRef p_format)
 				// Count the number of consecutive identical characters
 				unichar_t t_want = t_char;
 				int t_count = 1;
-				while ((t_char == MCStringGetCharAtIndex(p_format, t_offset++)) == t_want)
-					t_count++;
+				while ((t_char = MCStringGetCharAtIndex(p_format, t_offset)) == t_want)
+                {
+                    t_count++;
+                    t_offset++;
+                }
 				
 				// Append the correct formatting instruction
-				switch (t_char)
+				switch (t_want)
 				{
 					case 'h':
 						if (t_count == 1)
