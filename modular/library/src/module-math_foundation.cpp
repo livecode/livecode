ea2146614a9a8b36b2c03b0b4dc0c5998442ff9b
runrevali <ali@runrev.com>
2014-10-13 16:52:58 +0100

    Add math foundation module spec and implementation

---
+++ modular/library/src/module-math_foundation.cpp
@@ -0,0 +1,90 @@
+/* Copyright (C) 2003-2013 Runtime Revolution Ltd.
+ 
+ This file is part of LiveCode.
+ 
+ LiveCode is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License v3 as published by the Free
+ Software Foundation.
+ 
+ LiveCode is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
+
+#include <foundation.h>
+
+void MCMathFoundationExecRoundDownReal(double& x_target)
+{
+    x_target = floor(x_target + 0.5);
+}
+
+void MCMathFoundationExecRoundUpReal(double& x_target)
+{
+    x_target = ceil(x_target - 0.5);
+}
+
+void MCMathFoundationExecRoundToNearestReal(double& x_target)
+{
+	if (x_target < 0.0)
+		MCMathFoundationExecRoundUpReal(x_target);
+	else
+        MCMathFoundationExecRoundUpReal(x_target);
+}
+
+void MCMathFoundationExecRoundDownNumber(MCNumberRef& x_target)
+{
+    double t_target = MCNumberFetchAsReal(x_target);
+    MCMathFoundationExecRoundDownReal(t_target);
+    
+    MCValueRelease(x_target);
+    MCNumberCreateWithReal(t_target, x_target);
+}
+
+void MCMathFoundationExecRoundUpNumber(MCNumberRef& x_target)
+{
+    double t_target = MCNumberFetchAsReal(x_target);
+    MCMathFoundationExecRoundUpReal(t_target);
+    
+    MCValueRelease(x_target);
+    MCNumberCreateWithReal(t_target, x_target);
+}
+
+void MCMathFoundationExecRoundToNearestNumber(MCNumberRef& x_target)
+{
+    double t_target = MCNumberFetchAsReal(x_target);
+    MCMathFoundationExecRoundToNearestReal(t_target);
+    
+    MCValueRelease(x_target);
+    MCNumberCreateWithReal(t_target, x_target);
+}
+
+void MCMathFoundationEvalFloorReal(double& x_target)
+{
+    x_target = floor(x_target);
+}
+
+void MCMathFoundationEvalCeilingReal(double& x_target)
+{
+    x_target = ceil(x_target);
+}
+
+void MCMathFoundationEvalFloorNumber(MCNumberRef& x_target)
+{
+    double t_target = MCNumberFetchAsReal(x_target);
+    MCMathFoundationEvalFloorReal(t_target);
+    
+    MCValueRelease(x_target);
+    MCNumberCreateWithReal(t_target, x_target);
+}
+
+void MCMathFoundationEvalCeilingNumber(MCNumberRef& x_target)
+{
+    double t_target = MCNumberFetchAsReal(x_target);
+    MCMathFoundationEvalCeilingReal(t_target);
+    
+    MCValueRelease(x_target);
+    MCNumberCreateWithReal(t_target, x_target);
+}
