5e7ed5ca3b88f090dc1e82380fb7819a10767166
runrevali <ali@runrev.com>
2014-11-03 11:39:20 +0000

    Add encoding syntax and implementation to mlc std lib

---
+++ modular/library/src/module-encoding.cpp
@@ -0,0 +1,149 @@
+/* Copyright (C) 2003-2013 Runtime Revolution Ltd.
+ 
+ This file is part of LiveCode.
+ 
+ LiveCode is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License v3 as published by the Free
+ Software Foundation.
+ 
+ LiveCode is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
+
+#include <foundation.h>
+#include "foundation-auto.h"
+#include "foundation-filters.h"
+
+void MCEncodingEvalEncodedOfValue(MCValueRef p_target, MCDataRef& r_output)
+{
+    
+}
+
+void MCEncodingEvalDecodedOfValue(MCDataRef p_target, MCValueRef& r_output)
+{
+    
+}
+
+void MCEncodingExecCompress(MCDataRef& x_target)
+{
+    MCAutoDataRef t_compressed;
+    if (MCFiltersCompress(x_target, &t_compressed))
+    {
+        MCValueAssign(x_target, *t_compressed);
+        return;
+    }
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecDecompress(MCDataRef& x_target)
+{
+    MCAutoDataRef t_decompressed;
+    if (MCFiltersCompress(x_target, &t_decompressed))
+    {
+        MCValueAssign(x_target, *t_decompressed);
+        return;
+    }
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecEncodeUsingBase64(MCDataRef p_target, MCStringRef& r_output)
+{
+    if (MCFiltersBase64Encode(p_target, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecDecodeUsingBase64(MCStringRef p_target, MCDataRef& r_output)
+{
+    if (MCFiltersBase64Decode(p_target, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecEncodeUsingUTF8(MCStringRef p_target, MCDataRef& r_output)
+{
+    if (MCStringEncode(p_target, kMCStringEncodingUTF8, false, r_output))
+        return;
+    
+//    ctxt . Throw();
+}
+
+void MCEncodingExecDecodeUsingUTF8(MCDataRef p_target, MCStringRef& r_output)
+{
+    if (MCStringDecode(p_target, kMCStringEncodingUTF8, false, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecEncodeUsingUTF16(MCStringRef p_target, MCDataRef& r_output)
+{
+    if (MCStringEncode(p_target, kMCStringEncodingUTF16, false, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecDecodeUsingUTF16(MCDataRef p_target, MCStringRef& r_output)
+{
+    if (MCStringDecode(p_target, kMCStringEncodingUTF16, false, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecEncodeUsingUTF32(MCStringRef p_target, MCDataRef& r_output)
+{
+    if (MCStringEncode(p_target, kMCStringEncodingUTF32, false, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecDecodeUsingUTF32(MCDataRef p_target, MCStringRef& r_output)
+{
+    if (MCStringDecode(p_target, kMCStringEncodingUTF32, false, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecEncodeUsingASCII(MCStringRef p_target, MCDataRef& r_output)
+{
+    if (MCStringEncode(p_target, kMCStringEncodingASCII, false, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingExecDecodeUsingASCII(MCDataRef p_target, MCStringRef& r_output)
+{
+    if (MCStringDecode(p_target, kMCStringEncodingASCII, false, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingEvalURLEncoded(MCStringRef p_target, MCStringRef& r_output)
+{
+    if (MCFiltersUrlEncode(p_target, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
+
+void MCEncodingEvalURLDecoded(MCStringRef p_target, MCStringRef& r_output)
+{
+    if (MCFiltersUrlDecode(p_target, r_output))
+        return;
+    
+    //    ctxt . Throw();
+}
