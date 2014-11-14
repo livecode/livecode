c653d3d391e38b3aaa0f4954583947b194a7a1ca
Fraser J. Gordon <fraser.gordon@runrev.com>
2014-11-14 15:24:03 +0000

    Add no-op theme implementations for iOS, Android, Windows

--- engine/src/platform.cpp
+++ engine/src/platform.cpp
@@ -408,3 +408,30 @@ void MCPlatformCallbackSendSoundFinished(MCPlatformSoundRef p_sound)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+
+
+// No theming for mobile platforms yet
+#ifdef _MOBILE
+bool MCPlatformGetControlThemePropBool(MCPlatformControlType, MCPlatformControlPart, MCPlatformControlState, MCPlatformThemeProperty, bool&)
+{
+    return false;
+}
+
+bool MCPlatformGetControlThemePropInteger(MCPlatformControlType, MCPlatformControlPart, MCPlatformControlState, MCPlatformThemeProperty, int&)
+{
+    return false;
+}
+
+bool MCPlatformGetControlThemePropColor(MCPlatformControlType, MCPlatformControlPart, MCPlatformControlState, MCPlatformThemeProperty, MCColor&)
+{
+    return false;
+}
+
+bool MCPlatformGetControlThemePropFont(MCPlatformControlType, MCPlatformControlPart, MCPlatformControlState, MCPlatformThemeProperty, MCFontRef&)
+{
+    return false;
+}
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
---
+++ engine/src/windows-theme.cpp
@@ -0,0 +1,9 @@
+//
+//  windows-theme.cpp
+//  engine
+//
+//  Created by Fraser Gordon on 14/11/2014.
+//
+//
+
+#include <stdio.h>
