9afa4656f16fcedba49df24940f8d52d0539e4e0
Mark Waddingham <mark.waddingham@runrev.com>
2014-11-18 12:36:06 +0000

    [[ Foundation ]] Tweak initialize / finalize order.

--- libfoundation/src/foundation-core.cpp
+++ libfoundation/src/foundation-core.cpp
@@ -34,18 +34,18 @@ bool MCInitialize(void)
     
     if (!__MCValueInitialize())
 		return false;
-
+    
+	if (!__MCStringInitialize())
+		return false;
+    
+	if (!__MCNameInitialize())
+		return false;
+    
     if (!__MCErrorInitialize())
         return false;
 
     if (!__MCTypeInfoInitialize())
         return false;
-    
-	if (!__MCStringInitialize())
-		return false;
-
-	if (!__MCNameInitialize())
-		return false;
 
 	if (!__MCArrayInitialize())
 		return false;
@@ -75,11 +75,11 @@ void MCFinalize(void)
     __MCSetFinalize();
 	__MCListFinalize();
 	__MCArrayFinalize();
-	__MCNameFinalize();
-	__MCStringFinalize();
     __MCDataFinalize();
     __MCTypeInfoFinalize();
     __MCErrorFinalize();
+	__MCNameFinalize();
+	__MCStringFinalize();
 	__MCValueFinalize();
     __MCUnicodeFinalize();
 }
