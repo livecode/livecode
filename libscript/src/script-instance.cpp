d4a0cda360a1480d23f346cd0be1898dad95f686
Mark Waddingham <mark.waddingham@runrev.com>
2014-12-09 12:45:46 +0000

    [[ Script ]] Fix bytecode op argument decode / encode.

--- libscript/src/script-instance.cpp
+++ libscript/src/script-instance.cpp
@@ -539,13 +539,16 @@ static inline uindex_t MCScriptBytecodeDecodeArgument(byte_t*& x_bytecode_ptr)
 {
     uindex_t t_value;
     t_value = 0;
+    int t_shift;
+    t_shift = 0;
     for(;;)
     {
         byte_t t_next;
         t_next = *x_bytecode_ptr++;
-        t_value = (t_value << 7) | (t_next & 0x7f);
+        t_value |= (t_next & 0x7f) << t_shift;
         if ((t_next & 0x80) == 0)
             break;
+        t_shift += 7;
     }
     return t_value;
 }
@@ -662,7 +665,7 @@ static bool MCScriptPerformScriptInvoke(MCScriptFrame*& x_frame, byte_t*& x_next
     t_signature = p_instance -> module -> types[p_handler -> type] -> typeinfo;
     
     if (MCHandlerTypeInfoGetParameterCount(t_signature) != p_arity)
-        return MCScriptThrowWrongNumberOfArgumentsForInvokeError(x_frame -> instance -> module, x_frame -> address, p_handler, p_arity - 1);
+        return MCScriptThrowWrongNumberOfArgumentsForInvokeError(x_frame -> instance -> module, x_frame -> address, p_handler, p_arity);
     
     for(uindex_t i = 0; i < p_arity; i++)
     {
