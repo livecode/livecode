429a0180dedab83931aeed42e35f1ac28835dfc0
Peter TB Brett <peter.brett@livecode.com>
2014-12-11 10:54:20 +0000

    [[ Cleanup ]] MCExternalVariable: Avoid double-dereferencing pointers.
    Previously, MCExternalVariable's AppendInteger() and AppendCardinal()
    methods each cast their 32-bit integer argument to a pointer and
    dereferenced it, which was clearly the Wrong Thing on any non-32-bit
    platform.
    
    When I looked into this further, I found that both call sites for
    these methods passed in values, not pointers, so the previous
    behaviour was Obviously Wrong.
    
    If this change causes regressions then we'll need to fix the API to be
    64-bit safe and a lot less abusive.

--- engine/src/externalv1.cpp
+++ engine/src/externalv1.cpp
@@ -991,7 +991,7 @@ MCExternalError MCExternalVariable::AppendInteger(MCExternalValueOptions p_optio
 {
     MCAutoStringRef t_string;
     
-    if (!MCStringFormat(&t_string, "%d", *(int32_t *)p_value))
+    if (!MCStringFormat(&t_string, "%d", p_value))
         return kMCExternalErrorOutOfMemory;
     
 	return AppendString(p_options, *t_string);
@@ -1000,7 +1000,7 @@ MCExternalError MCExternalVariable::AppendInteger(MCExternalValueOptions p_optio
 MCExternalError MCExternalVariable::AppendCardinal(MCExternalValueOptions p_options, uint32_t p_value)
 {
     MCAutoStringRef t_string;
-	if (!MCStringFormat(&t_string, "%u", *(uint32_t *)p_value))
+	if (!MCStringFormat(&t_string, "%u", p_value))
         return kMCExternalErrorOutOfMemory;
     
 	return AppendString(p_options, *t_string);
