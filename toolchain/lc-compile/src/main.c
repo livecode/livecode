livecode/livecode

commit fe648150fced03fe9f0b2267b9d29ea73582be47

[[ Build ]] Linux: Pass -D_DEBUG during debug builds of compiler.

toolchain/Makefile
@@ -1,6 +1,9 @@
 top_srcdir=..
 
 include $(top_srcdir)/rules/environment.linux.makefile
+ifeq ($(MODE),debug)
+    CFLAGS += -D_DEBUG
+endif
 
 all: lc-compile
 

commit 16ffa9205e2239d5df301375d517535f28fcffb4

[[ Build ]] Linux: Bison debugging only available if YYDEBUG=1.
For debug builds, we need to define YYDEBUG as well as _DEBUG.  The
use of Bison debugging API should only be enabled if YYDEBUG is
defined.

This fixes a bootstrap failure bug introduced in commit fe648150fced.

toolchain/Makefile
@@ -2,7 +2,7 @@ top_srcdir=..
 
 include $(top_srcdir)/rules/environment.linux.makefile
 ifeq ($(MODE),debug)
-    CFLAGS += -D_DEBUG
+    CFLAGS += -D_DEBUG -DYYDEBUG=1
 endif
 
 all: lc-compile

toolchain/lc-compile/src/main.c
@@ -217,7 +217,7 @@ int main(int argc, char *argv[])
         argc -= 1;
         argv += 1;
         
-#ifdef _DEBUG
+#ifdef YYDEBUG
         yydebug = 1;
 #endif
     }