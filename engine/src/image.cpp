59e80448b270f9e71100134de637d794bfbf02ee
runrevpanos <panos.merakos@runrev.com>
2015-01-15 14:26:49 +0000

    [[ Bug 14347 ]] Unlock bitmap to prevent a crash

--- engine/src/image.cpp
+++ engine/src/image.cpp
@@ -1252,12 +1252,14 @@ Exec_stat MCImage::setprop(uint4 parid, Properties p, MCExecPoint &ep, Boolean e
 				if (t_success)
 					MCImageBitmapSet(t_copy, MCGPixelPackNative(0, 0, 0, 255)); // set to opaque black
 			}
-
+            
 			if (t_success)
 			{
 				MCImageSetMask(t_copy, (uint8_t*)data.getstring(), data.getlength(), true);
+                // PM-2015-01-15: [[ Bug 14347 ]] Unlock bitmap to prevent a crashgit
+                unlockbitmap(t_copy);
 				setbitmap(t_copy, 1.0);
-			}
+            }
 
 			MCImageFreeBitmap(t_copy);
 
