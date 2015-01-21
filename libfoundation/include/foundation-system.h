43231e4dc7b2ba61bd7a78ce5193b69561a2220f
Peter TB Brett <peter@peter-b.co.uk>
2015-01-21 15:26:42 +0000

    [[libsystem]] Add "foundation system" library infrastructure.
    * Top level header file
    * Initialization & finalization

---
+++ libfoundation/include/foundation-system.h
@@ -0,0 +1,31 @@
+/*                                                                     -*-c++-*-
+Copyright (C) 2015 Runtime Revolution Ltd.
+
+This file is part of LiveCode.
+
+LiveCode is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License v3 as published by the Free
+Software Foundation.
+
+LiveCode is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
+
+#ifndef __MCS_SYSTEM_H__
+#define __MCS_SYSTEM_H__
+#define __MCS_SYSTEM_H_INSIDE__
+
+#include <foundation.h>
+
+extern "C" {
+
+#include <system-init.h>
+
+}
+
+#undef __MCS_SYSTEM_H_INSIDE__
+#endif /* !__MC_SYSTEM_H__ */
