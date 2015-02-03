c536fcc1e02d3c50e6be3f3f9041be092c5c8d1c
Peter TB Brett <peter.brett@livecode.com>
2015-02-03 10:28:39 +0000

    [CID 15463] libcore: MCCStringFromUnicode(): Dereference after null check

--- libcore/src/core.cpp
+++ libcore/src/core.cpp
@@ -632,11 +632,14 @@ bool MCCStringToUnicode(const char *p_string, unichar_t*& r_unicode_string)
 
 bool MCCStringFromUnicode(const unichar_t *p_unicode_string, char*& r_string)
 {
+	if (NULL == p_unicode_string)
+		return false;
+
 	uint32_t t_wstring_length;
 	t_wstring_length = 0;
-	if (p_unicode_string != nil)
-		while(p_unicode_string[t_wstring_length] != 0)
-			t_wstring_length++;
+
+	while(p_unicode_string[t_wstring_length] != 0)
+		t_wstring_length++;
 	
 	return MCCStringFromUnicodeSubstring(p_unicode_string, t_wstring_length, r_string);
 }
