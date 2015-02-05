098edf8db3e4500e2c88ff4103c1657c46d5b2aa
Ian Macphail <ian.macphail@runrev.com>
2015-02-05 15:56:50 +0000

    [[ ModuleResources ]] Add new source files to windows project files. Add prefix header include to new source files.

--- engine/src/filepath.cpp
+++ engine/src/filepath.cpp
@@ -14,6 +14,8 @@
  You should have received a copy of the GNU General Public License
  along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
 
+#include "prefix.h"
+
 #include "foundation.h"
 #include "foundation-auto.h"
 
