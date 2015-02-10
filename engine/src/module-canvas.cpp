livecode/livecode

commit dbbfd1e27ebbbebef483ce722186ee3bdf6e07a6

Canvas: Implement __MCCanvasColorDescribe().
    <color: 0.0, 1.0, 0.5>

engine/src/module-canvas.cpp
@@ -427,6 +427,11 @@ bool MCCanvasJoinStyleFromString(MCStringRef p_string, MCGJoinStyle &r_style);
 bool MCCanvasCapStyleToString(MCGCapStyle p_style, MCStringRef &r_string);
 bool MCCanvasCapStyleFromString(MCStringRef p_string, MCGCapStyle &r_style);
 
+MCCanvasFloat MCCanvasColorGetRed(MCCanvasColorRef color);
+MCCanvasFloat MCCanvasColorGetGreen(MCCanvasColorRef color);
+MCCanvasFloat MCCanvasColorGetBlue(MCCanvasColorRef color);
+MCCanvasFloat MCCanvasColorGetAlpha(MCCanvasColorRef color);
+
 ////////////////////////////////////////////////////////////////////////////////
 
 static MCNameRef s_blend_mode_map[kMCGBlendModeCount];
@@ -880,8 +885,19 @@ static hash_t __MCCanvasColorHash(MCValueRef p_value)
 
 static bool __MCCanvasColorDescribe(MCValueRef p_value, MCStringRef &r_desc)
 {
-	// TODO - implement describe
-	return false;
+	MCCanvasColorRef t_color = static_cast<MCCanvasColorRef>(p_value);
+
+	if (1 < MCCanvasColorGetAlpha (t_color)) /* Opaque case */
+		return MCStringFormat (r_desc, "<color: %g, %g, %g>",
+		                       MCCanvasColorGetRed (t_color),
+		                       MCCanvasColorGetGreen (t_color),
+		                       MCCanvasColorGetBlue (t_color));
+	else
+		return MCStringFormat (r_desc, "<color: %g, %g, %g, %g>",
+		                       MCCanvasColorGetRed (t_color),
+		                       MCCanvasColorGetGreen (t_color),
+		                       MCCanvasColorGetBlue (t_color),
+		                       MCCanvasColorGetAlpha (t_color));
 }
 
 //////////

commit a59aafd942c3062623a5d926ad906510faf88093

Canvas: Allow opaque colors to be described without alpha.
Fix an incorrect comparison introduced in commit dbbfd1e.

engine/src/module-canvas.cpp
@@ -887,7 +887,7 @@ static bool __MCCanvasColorDescribe(MCValueRef p_value, MCStringRef &r_desc)
 {
 	MCCanvasColorRef t_color = static_cast<MCCanvasColorRef>(p_value);
 
-	if (1 < MCCanvasColorGetAlpha (t_color)) /* Opaque case */
+	if (1 <= MCCanvasColorGetAlpha (t_color)) /* Opaque case */
 		return MCStringFormat (r_desc, "<color: %g, %g, %g>",
 		                       MCCanvasColorGetRed (t_color),
 		                       MCCanvasColorGetGreen (t_color),