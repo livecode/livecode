53967729489cb8558dce1f015363b2e00a4164d6
runrevsebastien <sebastien@runrev.com>
2015-02-19 14:53:18 +0000

    [[ Bug 14625 ]] Only reinstate revsecurity.dylib creation for the simulator

--- tools/build-extension-ios.sh
+++ tools/build-extension-ios.sh
@@ -32,6 +32,13 @@ fi
 # SN-2015-02019: [[ Bug 14625 ]] We build and link each arch separately, and lipo them
 #  togother once it's done.
 
+# We still want to produce revsecurity.dylib for the simulator
+if [ "${SDK_NAME:0:8}" != "iphoneos" -a "${EXECUTABLE_NAME}" = "revsecurity" ]; then
+	BUILD_DYLIB=1
+else
+	BUILD_DYLIB=0
+fi
+
 LCEXT_FILE_LIST=""
 DYLIB_FILE_LIST=""
 
@@ -42,15 +49,20 @@ do
     DYLIB_FILE="$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.dylib_${ARCH}"
 
     # arm64 is only from iOS 7.0.0
-    if [ ${ARCH} = "arm64" ]; then
+    if [ ${ARCH} = "arm64" -o ${ARCH} = "x86_64" ]; then
 		MIN_VERSION="7.0"
     else
 		MIN_VERSION="5.1"
     fi
 
-	# We still want to produce revsecurity.dylib
-	if [ -n "$DYLIB_REQUIRED" ]; then
-		$BIN_DIR/g++ -dynamiclib -arch ${ARCH} -miphoneos-version-min=${MIN_VERSION} -isysroot $SDKROOT -L"$SOLUTION_DIR/prebuilt/lib/ios/$SDK_NAME" -o "${DYLIB_FILE}" "$BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME" -dead_strip -Wl,-x $SYMBOLS $DEPS
+	if [ $BUILD_DYLIB -eq 1 ]; then
+		OUTPUT=$($BIN_DIR/g++ -dynamiclib -arch ${ARCH} -miphoneos-version-min=${MIN_VERSION} -isysroot $SDKROOT -L"$SOLUTION_DIR/prebuilt/lib/ios/$SDK_NAME" -o "${DYLIB_FILE}" "$BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME" -dead_strip -Wl,-x $SYMBOLS $DEPS)
+		
+		if [ -n "$OUTPUT" ]; then
+			echo "Linking ""${DYLIB_FILE}""failed:"
+			echo $OUT
+			exit -1
+		fi
 	fi
     
     OUTPUT=$($BIN_DIR/g++ -nodefaultlibs $STRIP_OPTIONS -arch ${ARCH} -miphoneos-version-min=${MIN_VERSION} -isysroot $SDKROOT -L"$SOLUTION_DIR/prebuilt/lib/ios/$SDK_NAME" -o "${LCEXT_FILE}" "$BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME" -Wl,-sectcreate -Wl,__MISC -Wl,__deps -Wl,"$SRCROOT/$PRODUCT_NAME.ios" -Wl,-exported_symbol -Wl,___libinfoptr_$PRODUCT_NAME $STATIC_DEPS)
@@ -71,7 +83,7 @@ lipo -create ${LCEXT_FILE_LIST} -output "$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.lcext
 # Cleanup the lcext_$ARCH files generated
 rm ${LCEXT_FILE_LIST}
 
-if [ -n "$DYLIB_REQUIRED" ]; then
+if [ $BUILD_DYLIB -eq 1 ]; then
 	lipo -create ${DYLIB_FILE_LIST} -output "$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.dylib"
 	rm ${DYLIB_FILE_LIST}
 fi
