cc09e546eed8c821ca54bba09e7b72336ef8839e
Peter TB Brett <peter.brett@livecode.com>
2015-03-05 13:46:27 +0000

    com.livecode.bitwise: Refactor bit shifts.
    Require people to specify "left" or "right", and force the shift count
    to be positive.

--- libscript/src/module-bitwise.cpp
+++ libscript/src/module-bitwise.cpp
@@ -36,27 +36,32 @@ extern "C" MC_DLLEXPORT void MCBitwiseEvalBitwiseNot(integer_t p_operand, intege
     r_output = ~((int32_t)p_operand);
 }
 
-extern "C" MC_DLLEXPORT void MCBitwiseEvalBitwiseShift(integer_t p_operand, bool p_is_right, integer_t p_shift, integer_t& r_output)
+extern "C" MC_DLLEXPORT void MCBitwiseEvalBitwiseShift(integer_t p_operand, bool p_is_right, uinteger_t p_shift, integer_t& r_output)
 {
-	/* Ensure that the shift amount is always positive */
-	if (p_shift < 0)
-	{
-		/* Overflow check (why would anyone do this?) */
-		if (INTEGER_MIN == p_shift)
-			++p_shift;
-
-		p_shift = -p_shift;
-		p_is_right = !p_is_right;
-	}
-
-	/* Shifting integer_t by more than 31 bits is undefined */
-	integer_t t_max_shift = (sizeof(integer_t) << 3) - 1;
+	/* Maximum shift amount for which the C operator is defined */
+	uinteger_t t_max_shift = (sizeof(integer_t) << 3) - 1;
 	p_shift = MCMin (p_shift, t_max_shift);
 
+	integer_t t_shifted;
 	if (p_is_right)
-		r_output = p_operand >> p_shift;
+	{
+		/* It's okay to shift right by any amount */
+		t_shifted = p_operand >> p_shift;
+	}
 	else
-		r_output = p_operand << p_shift;
+	{
+		t_shifted = p_operand << p_shift;
+
+		/* Overflow check */
+		if (p_operand != t_shifted >> p_shift)
+		{
+			MCErrorCreateAndThrow (kMCGenericErrorTypeInfo, "reason",
+			                       MCSTR("overflow in bitwise operation"), nil);
+			return;
+		}
+	}
+
+	r_output = t_shifted;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
