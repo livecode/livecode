1f4dd2f6a760afa4164c53c10be51c54ad60b64e
Peter TB Brett <peter.brett@livecode.com>
2015-03-06 13:06:02 +0000

    com.livecode.list: Add "offset of _ before _ in _" accessor.
    Add a new operation on lists:
        
       the offset of <tNeedle> before <tPosition> in <tList>
        
    This looks for an subsequence equal to <tNeedle> in the subsequence of
    <tList> ending at the 1-based index <tPosition> (exclusive).  It
    returns the 1-based index in <tList> of the first element of the
    matching subsequence, or 0 if no matching subsequence is found.
        
    There are two variants of the operation:
        
        the first offset of <tNeedle> before <tPosition> in <tList>
        the last offset of <tNeedle> before <tPosition> in <tList>
        
    <tNeedle> must be a List.

--- libscript/src/module-list.cpp
+++ libscript/src/module-list.cpp
@@ -589,6 +589,30 @@ MCListEvalOffsetOfListAfter (bool p_is_last,
 	                               r_output);
 }
 
+extern "C" MC_DLLEXPORT void
+MCListEvalOffsetOfListBefore (bool p_is_last,
+                              MCProperListRef p_needle,
+                              index_t p_before,
+                              MCProperListRef p_haystack,
+                              uindex_t & r_output)
+{
+	uindex_t t_start, t_count;
+	if (p_before == 0)
+	{
+		t_start = UINDEX_MAX;
+	} else if (!MCChunkGetExtentsOfElementChunkByExpressionInRange (p_haystack,
+	                nil, p_before, true, false, true, t_start, t_count))
+	{
+		MCErrorCreateAndThrow (kMCGenericErrorTypeInfo, "reason",
+		                       MCSTR("chunk index out of range"), nil);
+		return;
+	}
+
+	MCListEvalOffsetOfListInRange (p_is_last, p_needle, p_haystack,
+	                               MCRangeMake(0, t_start),
+	                               r_output);
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifdef _TEST
