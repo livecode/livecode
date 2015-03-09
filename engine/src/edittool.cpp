5d4ab05cd7ef5918681a604cd3a2aa8add74efe7
Peter TB Brett <peter.brett@livecode.com>
2015-03-09 12:34:48 +0000

    [CID 15180] MCPolygonEditTool::handler_under_point(): Free using delete[]

--- engine/src/edittool.cpp
+++ engine/src/edittool.cpp
@@ -320,11 +320,11 @@ uint4 MCPolygonEditTool::handle_under_point(int2 x, int2 y)
 	for (uint4 i=0; i<npts; i++)
 		if (MCU_point_in_rect(rects[i], x, y))
 		{
-			delete rects;
+			delete[] rects;
 			return i;
 		}
 
-	delete rects;
+	delete[] rects;
 	return -1;
 }
 
