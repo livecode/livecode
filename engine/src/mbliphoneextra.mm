fd1d8ef7d9238cb9c12ca17971ea19f1b73d81ba
runrevpanos <panos.merakos@runrev.com>
2015-03-31 11:40:30 +0100

    [Bug 14333] Nil-check for MCtargetptr

--- engine/src/mbliphoneextra.mm
+++ engine/src/mbliphoneextra.mm
@@ -1078,7 +1078,8 @@ static Exec_stat MCHandleClearTouches(void *context, MCParameter *p_parameters)
 	MCEventQueueClearTouches();
     
     // PM-2015-03-16: [[ Bug 14333 ]] Make sure the object that triggered a mouse down msg is not focused, as this stops later mouse downs from working
-    MCtargetptr -> munfocus();
+    if (MCtargetptr != nil)
+        MCtargetptr -> munfocus();
     
 	return ES_NORMAL;
 }
