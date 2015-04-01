a83e643879c9195583bfa69fc08f8600180db0a3
Mark Waddingham <mark.waddingham@runrev.com>
2015-04-01 09:36:48 +0100

    [[ Bug 15128 ]] Fix OnMouseMove invocation in widgets.
    We now only update the event managers mouse x and y variables on sending a mouse move event. This ensures that the correct widget handles the mouse move.

--- engine/src/widget-events.cpp
+++ engine/src/widget-events.cpp
@@ -172,10 +172,6 @@ Boolean MCWidgetEventManager::event_mfocus(MCWidget* p_widget, int2 p_x, int2 p_
     bool t_pos_changed;
     t_pos_changed = !(p_x == m_mouse_x && p_y == m_mouse_y);
     
-    // Update the mouse position
-    m_mouse_x = p_x;
-    m_mouse_y = p_y;
-    
     // Do a quick bounds test on the targeted widget. If this fails, the widget
     // wasn't the target of the mouse focus event.
     MCRectangle p_rect;
@@ -197,6 +193,10 @@ Boolean MCWidgetEventManager::event_mfocus(MCWidget* p_widget, int2 p_x, int2 p_
         
         if (m_mouse_grab == p_widget)
         {
+            // Update the mouse position
+            m_mouse_x = p_x;
+            m_mouse_y = p_y;
+            
             mouseMove(p_widget, p_x, p_y);
             return true;
         }
@@ -214,6 +214,10 @@ Boolean MCWidgetEventManager::event_mfocus(MCWidget* p_widget, int2 p_x, int2 p_
     }
     else if (t_pos_changed)
     {
+        // Update the mouse position
+        m_mouse_x = p_x;
+        m_mouse_y = p_y;
+        
         // Mouse has moved within this widget
         mouseMove(p_widget, p_x, p_y);
     }
