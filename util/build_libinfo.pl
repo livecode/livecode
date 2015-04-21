3a0fafa409a248c633e146320670c2af839cc9cb
Peter TB Brett <peter.brett@livecode.com>
2015-04-21 15:56:41 +0100

    build_libinfo.pl: Input/output using files instead of stdio streams

--- util/build_libinfo.pl
+++ util/build_libinfo.pl
@@ -15,8 +15,18 @@ sub output;
 # Parameters
 my $name = $ARGV[0];
 
-# Read in from stdin
-my @spec = <STDIN>;
+my $inputFile = $ARGV[1];
+my $outputFile = $ARGV[2];
+
+# Read all of the input data
+open INPUT, "<$inputFile"
+	or die "Could not open input file \"$inputFile\": $!";
+my @spec = <INPUT>;
+close INPUT;
+
+# Open the output file
+open OUTPUT, ">$outputFile"
+	or die "Could not open output file \"$outputFile\": $!";
 
 # Prefix for symbols
 my $prefix = "";
@@ -36,23 +46,23 @@ foreach my $line (@spec)
 	{
 		next;
 	}
-	
+
 	# Ignore comment lines
 	if ($line =~ /^\s*#/)
 	{
 		next;
 	}
-	
+
 	# Ignore lines defining modules (we only care about symbols)
 	if (substr($line, 0, 1) ne "\t")
 	{
 		next;
 	}
-	
+
 	# Skip the tab character
 	substr($line, 0, 1) = "";
 	trim($line);
-	
+
 	# Handle optional symbols
 	my $symbol = undef;
 	my @words = split('\s+', $line);
@@ -64,21 +74,21 @@ foreach my $line (@spec)
 	{
 		$symbol = $words[0];
 	}
-	
+
 	# Remove any trailing colons
 	$symbol =~ s/:$// ;
-	
+
 	$symbolExterns .= "extern \"C\" void *$symbol;\n";
 	$symbolEntries .= "    { \"$symbol\", $symbol },\n";
 }
 
-print STDOUT $symbolExterns;
+print OUTPUT $symbolExterns;
 output "struct LibExport { const char *name; void *address; };";
 output "struct LibInfo { const char **name; struct LibExport *exports; };";
 output "static const char *__libexternalname = \"$name\";";
 output "static struct LibExport __libexports[] =";
 output "{";
-print STDOUT $symbolEntries;
+print OUTPUT $symbolEntries;
 output "  { 0, 0 }";
 output "};";
 output "static struct LibInfo __libinfo=";
@@ -95,6 +105,6 @@ sub output
 	{
 		$line = "";
 	}
-	
-	print STDOUT "$line\n";
+
+	print OUTPUT "$line\n";
 }
