0994d19b2a1626d0e2bc33ac6d5165370563e8d3
Fraser J. Gordon <fraser.gordon@livecode.com>
2015-04-28 13:26:22 +0100

    Use $OBJCOPY and $STRIP env vars when extracting debug symbols

--- tools/extract-debug-symbols.sh
+++ tools/extract-debug-symbols.sh
@@ -6,22 +6,25 @@ suffix="$1"
 shift
 inputs="$@"
 
+OBJCOPY="${OBJCOPY:-objcopy}"
+STRIP="${STRIP:-strip}"
+
 for input in $inputs ; do
 	output="${input}${suffix}"	
 
 	# Extract a copy of the debugging information
-	objcopy --only-keep-debug "$input" "$output" 
+	$OBJCOPY --only-keep-debug "$input" "$output" 
 	
 	# If this file is a dynamic library, don't do a full strip or we'll
 	# destroy it (no symbols => can't link to it)
 	if [[ "$(objdump -f $input)" =~ "DYNAMIC" ]] ; then
-		strip --preserve-dates --strip-debug "$input"
+		$STRIP --preserve-dates --strip-debug "$input"
 	else
-		strip --preserve-dates --strip-debug --strip-unneeded "$input"
+		$STRIP --preserve-dates --strip-debug --strip-unneeded "$input"
 	fi
 	
 	# Add a hint for the debugger so it can find the debug info
-	objcopy --remove-section=.gnu_debuglink "$input"
-	objcopy --preserve-dates --add-gnu-debuglink="$output" "$input"
+	$OBJCOPY --remove-section=.gnu_debuglink "$input"
+	$OBJCOPY --preserve-dates --add-gnu-debuglink="$output" "$input"
 done
 
