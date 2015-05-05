livecode/livecode

commit 79b29000c9e320f58dc043ef5821125b538fdd0a

[[ BUg 14467 ]] Refactor the whole MCChunk::getprop / setprop since they are fairly identical

engine/src/chunk.cpp
@@ -4205,97 +4205,23 @@ static MCPropertyInfo *lookup_object_property(const MCObjectPropertyTable *p_tab
 	return nil;
 }
 
-// SN-2015-02-13: [[ Bug 14467 ]] [[ Bug 14053 ]] Refactored
-//  object properties lookup, to ensure it is done the same
-//  way in MCChunk::getprop and MCChunk::setprop
-MCPropertyInfo *MCChunk::lookup_obj_prop(const MCObjectPropertyTable *p_table, Properties p_which, bool p_effective, bool p_array_prop)
-{
-    MCPropertyInfo *t_info;
-    
-    t_info = lookup_object_property(p_table, p_which, p_effective, p_array_prop, islinechunk() ? kMCPropertyInfoChunkTypeLine : kMCPropertyInfoChunkTypeChar);
-    
-    // If we could not get the line property for this chunk, then we try to get the char prop.
-    // If we could not get the char property for this chunk, then we try to get the line prop.
-    if (t_info == nil)
-        t_info = lookup_object_property(p_table, p_which, p_effective, p_array_prop, islinechunk() ? kMCPropertyInfoChunkTypeChar : kMCPropertyInfoChunkTypeLine);
-    
-    return t_info;
-}
-
-// MW-2011-11-23: [[ Array Chunk Props ]] If index is not nil, then treat as an array chunk prop
-bool MCChunk::getprop(MCExecContext& ctxt, Properties which, MCNameRef index, Boolean effective, MCExecValue& r_value)
+// SN-2015-02-13: [[ Bug 14467 ]] [[ Bug 14053 ]] Refactored object properties
+//  lookup, to ensure it is done the same way in MCChunk::getprop / setprop
+bool MCChunk::applyPropertyOperation(MCExecContext &ctxt, Properties which, MCNameRef index, Boolean effective, bool p_is_get_operation, MCExecValue &r_value)
 {
     MCObjectChunkPtr t_obj_chunk;
     if (evalobjectchunk(ctxt, false, false, t_obj_chunk) != ES_NORMAL)
         return false;
-
+    
     MCPropertyInfo *t_info;
     
     if (t_obj_chunk . chunk == CT_UNDEFINED)
     {
         bool t_success;
-        t_success = t_obj_chunk . object -> getprop(ctxt, t_obj_chunk . part_id, which, index, effective, r_value);
-        MCValueRelease(t_obj_chunk . mark . text);
-        return t_success;
-    }
-    else
-	{
-        // AL-2014-07-09: [[ Bug 12733 ]] Buttons are also valid containers wrt text chunk properties.
-		if (t_obj_chunk . object -> gettype() != CT_FIELD &&
-            t_obj_chunk . object -> gettype() != CT_BUTTON)
-		{
-			MCeerror->add(EE_CHUNK_BADCONTAINER, line, pos);
-            MCValueRelease(t_obj_chunk . mark . text);
-			return false;
-		}
-        
-        // MW-2011-11-23: [[ Array Chunk Props ]] If index is nil or empty, then its just a normal
-        //   prop, else its an array prop.
-        bool t_is_array_prop;
-        t_is_array_prop = (index != nil && !MCNameIsEmpty(index));
-        
-        // SN-2015-02-13: [[ Bug 14467 ]] [[ Bug 14053 ]] Refactor
-        //  identical way of looking up object properties in
-        //  MCChunk::getprop and MCChunk::setprop
-        t_info = lookup_obj_prop(t_obj_chunk . object -> getpropertytable(), which, effective == True, t_is_array_prop);
-        
-        if (t_info == nil || t_info -> getter == nil)
-        {
-            MCeerror -> add(EE_OBJECT_GETNOPROP, line, pos);
-            MCValueRelease(t_obj_chunk . mark . text);
-            return false;
-        }
-        
-        if (t_is_array_prop)
-        {
-            MCObjectChunkIndexPtr t_obj_chunk_index;
-            t_obj_chunk_index . object = t_obj_chunk . object;
-            t_obj_chunk_index . part_id = t_obj_chunk . part_id;
-            t_obj_chunk_index . chunk = t_obj_chunk . chunk;
-            t_obj_chunk_index . mark = t_obj_chunk . mark;
-            t_obj_chunk_index . index = index;
-            MCExecFetchProperty(ctxt, t_info, &t_obj_chunk_index, r_value);
-        }
+        if (p_is_get_operation)
+            t_success = t_obj_chunk . object -> getprop(ctxt, t_obj_chunk . part_id, which, index, effective, r_value);
         else
-            MCExecFetchProperty(ctxt, t_info, &t_obj_chunk, r_value);
-	}
-    
-    MCValueRelease(t_obj_chunk . mark . text);
-    return !ctxt . HasError();
-}
-
-// MW-2011-11-23: [[ Array Chunk Props ]] If index is not nil, then treat as an array chunk prop
-bool MCChunk::setprop(MCExecContext& ctxt, Properties which, MCNameRef index, Boolean effective, MCExecValue p_value)
-{
-    MCObjectChunkPtr t_obj_chunk;
-    if (evalobjectchunk(ctxt, false, true, t_obj_chunk) != ES_NORMAL)
-        return false;
-    
-    MCPropertyInfo *t_info;
-    
-    if (t_obj_chunk . chunk == CT_UNDEFINED)
-    {
-        bool t_success = t_obj_chunk . object -> setprop(ctxt, t_obj_chunk . part_id, which, index, effective, p_value);
+            t_success = t_obj_chunk . object -> setprop(ctxt, t_obj_chunk . part_id, which, index, effective, r_value);
         MCValueRelease(t_obj_chunk . mark . text);
         return t_success;
     }
@@ -4309,28 +4235,29 @@ bool MCChunk::setprop(MCExecContext& ctxt, Properties which, MCNameRef index, Bo
             MCValueRelease(t_obj_chunk . mark . text);
             return false;
         }
-    
+        
         // MW-2011-11-23: [[ Array Chunk Props ]] If index is nil or empty, then its just a normal
         //   prop, else its an array prop.
         bool t_is_array_prop;
         t_is_array_prop = (index != nil && !MCNameIsEmpty(index));
         
-        // SN-2015-02-13: [[ Bug 14467 ]] [[ Bug 14053 ]] Refactor
-        //  identical way of looking up object properties in
-        //  MCChunk::getprop and MCChunk::setprop
-        t_info = lookup_obj_prop(t_obj_chunk . object -> getpropertytable(), which, effective == True, t_is_array_prop);
+        t_info = lookup_object_property(t_obj_chunk . object -> getpropertytable(), which, effective == True, t_is_array_prop, islinechunk() ? kMCPropertyInfoChunkTypeLine : kMCPropertyInfoChunkTypeChar);
         
-        if (t_info == nil || t_info -> setter == nil)
+        // If we could not get the line property for this chunk, then we try to get the char prop.
+        // If we could not get the char property for this chunk, then we try to get the line prop.
+        if (t_info == nil)
+            t_info = lookup_object_property(t_obj_chunk . object -> getpropertytable(), which, effective == True, t_is_array_prop, islinechunk() ? kMCPropertyInfoChunkTypeChar : kMCPropertyInfoChunkTypeLine);
+        
+        if (t_info == nil || t_info -> getter == nil)
         {
-            MCeerror -> add(EE_OBJECT_SETNOPROP, line, pos);
+            if (p_is_get_operation)
+                MCeerror -> add(EE_OBJECT_GETNOPROP, line, pos);
+            else
+                MCeerror -> add(EE_OBJECT_SETNOPROP, line, pos);
+            
             MCValueRelease(t_obj_chunk . mark . text);
             return false;
         }
-        
-        // MW-2011-11-23: [[ Array TextStyle ]] Pass the 'index' along to method to
-        //   handle specific styles.
-        // MW-2012-01-25: [[ ParaStyles ]] Pass whether this was an explicit line chunk
-        //   or not. This is used to disambiguate the setting of 'backColor'.
         
         if (t_is_array_prop)
         {
@@ -4340,16 +4267,23 @@ bool MCChunk::setprop(MCExecContext& ctxt, Properties which, MCNameRef index, Bo
             t_obj_chunk_index . chunk = t_obj_chunk . chunk;
             t_obj_chunk_index . mark = t_obj_chunk . mark;
             t_obj_chunk_index . index = index;
-            
-            MCExecStoreProperty(ctxt, t_info, &t_obj_chunk_index, p_value);
+            if (p_is_get_operation)
+                MCExecFetchProperty(ctxt, t_info, &t_obj_chunk_index, r_value);
+            else
+                MCExecStoreProperty(ctxt, t_info, &t_obj_chunk_index, r_value);
         }
         else
-            MCExecStoreProperty(ctxt, t_info, &t_obj_chunk, p_value);
+        {
+            if (p_is_get_operation)
+                MCExecFetchProperty(ctxt, t_info, &t_obj_chunk, r_value);
+            else
+                MCExecStoreProperty(ctxt, t_info, &t_obj_chunk, r_value);
+        }
     }
-
-    MCValueRelease(t_obj_chunk . mark. text);
-
-    if (!ctxt . HasError())
+    
+    MCValueRelease(t_obj_chunk . mark . text);
+    
+    if (!p_is_get_operation && !ctxt . HasError())
     {
         // MM-2012-09-05: [[ Property Listener ]] Make sure any listeners are updated of the property change.
         //  Handled at this point rather than MCProperty::set as here we know if it is a valid object set prop.
@@ -4357,7 +4291,21 @@ bool MCChunk::setprop(MCExecContext& ctxt, Properties which, MCNameRef index, Bo
         return true;
     }
     
-    return false;
+    return !ctxt . HasError();
+}
+
+// MW-2011-11-23: [[ Array Chunk Props ]] If index is not nil, then treat as an array chunk prop
+bool MCChunk::getprop(MCExecContext& ctxt, Properties which, MCNameRef index, Boolean effective, MCExecValue& r_value)
+{
+    // SN-2015-02-13: [[ Bug 14467 ]] Object property getting / setting refactored
+    return applyPropertyOperation(ctxt, which, index, effective, true, r_value);
+}
+
+// MW-2011-11-23: [[ Array Chunk Props ]] If index is not nil, then treat as an array chunk prop
+bool MCChunk::setprop(MCExecContext& ctxt, Properties which, MCNameRef index, Boolean effective, MCExecValue p_value)
+{
+    // SN-2015-02-13: [[ Bug 14467 ]] Object property getting / setting refactored
+    return applyPropertyOperation(ctxt, which, index, effective, false, p_value);
 }
 
 Chunk_term MCChunk::getlastchunktype(void)

engine/src/chunk.h
@@ -171,6 +171,10 @@ public:
 
     // SN-2015-02-13: [[ Bug 14467 ]] Refactor the way object properties are looked up
     MCPropertyInfo *lookup_obj_prop(const MCObjectPropertyTable *p_table, Properties p_which, bool p_effective, bool p_array_prop);
+
+    // SN-2015-02-13: [[ Bug 14467 ]] [[ Bug 14053 ]] Refactored object properties
+    //  lookup, to ensure it is done the same way in MCChunk::getprop / setprop
+    bool applyPropertyOperation(MCExecContext& ctxt, Properties which, MCNameRef index, Boolean effective, bool p_is_get_operation, MCExecValue& r_value);
     
     bool getprop(MCExecContext& ctxt, Properties which, MCNameRef index, Boolean effective, MCExecValue& r_value);
     bool setprop(MCExecContext& ctxt, Properties which, MCNameRef index, Boolean effective, MCExecValue p_value);

commit c39ef1e4a4de5e4165abb9f6ed98784b60fc5b91

[[ Bug 13313 Reopen ]] Fix delimiter issue introduced in 79b29000c9e320f58dc043ef5821125b538fdd0a

engine/src/chunk.cpp
@@ -4213,7 +4213,9 @@ static MCPropertyInfo *lookup_object_property(const MCObjectPropertyTable *p_tab
 bool MCChunk::getsetprop(MCExecContext &ctxt, Properties which, MCNameRef index, Boolean effective, bool p_is_get_operation, MCExecValue &r_value)
 {
     MCObjectChunkPtr t_obj_chunk;
-    if (evalobjectchunk(ctxt, false, false, t_obj_chunk) != ES_NORMAL)
+    // SN-2015-05-05: [[ Bug 13314 Reopen ]] We force the chunk delimiter
+    //  existence when setting a string value.
+    if (evalobjectchunk(ctxt, false, !p_is_get_operation, t_obj_chunk) != ES_NORMAL)
         return false;
     
     MCPropertyInfo *t_info;