360d94a00e0a5f00636011109732039dc9233c62
Stephen Tawn <stephen.tawn@livecode.com>
2015-05-19 09:22:37 +0100

    Add leak detection code.
    This is a hack and should never be merged into the main tree!

---
+++ engine/src/__leak.cxx
@@ -0,0 +1,1 @@
+/home/fedora/__leak.cpp
--- engine/src/srvmain.cpp
+++ engine/src/srvmain.cpp
@@ -610,6 +610,9 @@ extern void MCModulesFinalize();
 
 int main(int argc, char *argv[], char *envp[])
 {
+	void initialize_mem_dbg();
+	initialize_mem_dbg();
+	
 	if (!MCInitialize() || !MCSInitialize() ||
 	    !MCModulesInitialize() || !MCScriptInitialize())
 		exit(-1);
@@ -667,6 +670,12 @@ int main(int argc, char *argv[], char *envp[])
 		MCValueRelease(t_new_envp[i]);
 	}
 	MCMemoryDeleteArray(t_new_envp);
+
+	void report_mem_dbg();
+	report_mem_dbg();
+	
+	void restore_handlers();
+	restore_handlers();
 
 	exit(t_exit_code);
 }
