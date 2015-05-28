b9d034ec76e62510e8e8121e1936785fc53c8891
Fraser J. Gordon <fraser.gordon@livecode.com>
2015-05-28 15:58:34 +0100

    Improve invokation of Unix utilities when running within Wine

--- util/invoke-unix.bat
+++ util/invoke-unix.bat
@@ -4,8 +4,17 @@ REM This batch file forwards commands to a Unix-like shell
 REM
 REM We need to work around what looks to be a MCVS bug: the final parameter is
 REM missing the terminating '"' when quoted
+SET commands=%*"
 
-@echo Invoking Unix command '%*"'
+@echo Invoking Unix command '%commands%'
+
+REM Does this look like a wine environment?
+REM We guess this by looking for a common-ish Wine env var
+IF DEFINED WINEDEBUG (
+  START /wait %~dp0\invoke-unix-wine.exe %commands%
+  EXIT %ERRORLEVEL%
+)
+
 
 IF EXIST C:\Cygwin\bin (
   REM Obscure way to get the cmd.exe equivalent to `...` substitution
@@ -13,9 +22,9 @@ IF EXIST C:\Cygwin\bin (
   FOR /F "usebackq tokens=*" %%x IN (`C:\Cygwin\bin\cygpath.exe %*"`)  DO SET cygwin_cmd=!cygwin_cmd! %%x
 
   C:\Cygwin\bin\bash.exe -c 'cd !cygwin_cd! ^&^& !cygwin_cmd!'
-) ELSE (
-  invoke-unix-wine.exe %*"
+  EXIT %ERRORLEVEL%
 )
 
-EXIT %ERRORLEVEL%
+@echo Could not find Cygwin or wine
+EXIT 1
 
