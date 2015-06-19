livecode/livecode

commit e107048bd68c834caa2da2119299963fe554c632

[[ Bug 15474 ]] Make sure that integer, literal number are parsed properly on 64-bit Linux

libfoundation/src/foundation-number.cpp
@@ -143,7 +143,15 @@ bool MCNumberParseOffset(MCStringRef p_string, uindex_t offset, uindex_t char_co
         char *t_end;
         // SN-2014-10-06: [[ Bug 13594 ]] We want an unsigned integer if possible
         uinteger_t t_uinteger;
+#ifdef __LP64__
+        unsigned long t_ulong;
+        t_ulong = strtoul(t_chars, &t_end, 10);
+        if (t_ulong > UINTEGER_MAX)
+            errno = ERANGE;
+        t_uinteger = (uinteger_t) t_ulong;
+#elif __LP32__ || __LLP64__
         t_uinteger = strtoul(t_chars, &t_end, 10);
+#endif
         
         // AL-2014-07-31: [[ Bug 12936 ]] Check the right number of chars has been consumed
         // SN-2014-10-06: [[ Bug 13594 ]] Also check that no error was encountered

commit dc17959eaddd0f2329efa3f4b56e176f7fa927a2

[[Bug 15527]] libfoundation: Fix build on __LP32__ platforms.
Correctly test whether preprocessor macros are defined.  Build failure
introduced in commit e107048b.

libfoundation/src/foundation-number.cpp
@@ -146,13 +146,13 @@ bool __MCNumberParseNativeString(const char *p_string, uindex_t p_length, bool p
     t_end  = nil;
     // SN-2014-10-06: [[ Bug 13594 ]] We want an unsigned integer if possible
     uinteger_t t_uinteger;
-#ifdef __LP64__
+#if defined(__LP64__)
     unsigned long t_ulong;
     t_ulong = strtoul(t_string, &t_end, t_base);
     if (t_ulong > UINTEGER_MAX)
         errno = ERANGE;
     t_uinteger = (uinteger_t) t_ulong;
-#elif __LP32__ || __LLP64__
+#elif defined(__LP32__) || defined(__LLP64__)
     t_uinteger = strtoul(t_string, &t_end, t_base);
 #endif
     