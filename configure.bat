6170378e1cccda2cb5cc02fb50c7b7a8c8c334dd
Fraser J. Gordon <fraser.gordon@livecode.com>
2015-06-23 15:17:51 +0100

    Win32 configure.bat gives errors/warnings for missing SDKs
    A mising QuickTime SDK will result in an error while missing speech
    SDKs will warn that revSpeech will not build.

--- configure.bat
+++ configure.bat
@@ -3,6 +3,16 @@ REM Configures the Windows build
 
 SETLOCAL EnableExtensions
 
+REM Set this variable if any warnings have been reported
+SET warnings=0
+
+REM Not all versions of windows the the %programfiles(x86)% variable
+IF NOT DEFINED programfiles(x86) SET programfiles(x86)=%programfiles%
+
+REM Note: to test whether a directory exists in batch script, you need to check
+REM whether a file within that directory exists. Easiest way to do this is to
+REM add the "*" wildcard after the directory
+
 REM Attempt to locate a copy of Perl
 WHERE /Q perl 1>NUL 2>NUL
 IF %ERRORLEVEL% NEQ 0 (
@@ -30,6 +40,34 @@ IF %ERRORLEVEL% NEQ 0 (
 ) ELSE (
   SET python=python
 )
+
+REM Attempt to locate the QuickTime SDK
+IF EXIST "%programfiles(x86)%\QuickTime SDK\*" (
+  SET extra_options=%extra_options% -Dquicktime_sdk="%programfiles(x86)%/QuickTime SDK"
+) ELSE (
+  ECHO >&2 Error: could not locate the QuickTime SDK
+  PAUSE
+  EXIT 1
+)
+
+REM Attempt to locate the Microsoft Speech SDK v5.1
+IF EXIST "%programfiles(x86)%\Microsoft Speech SDK 5.1\*" (
+  SET extra_options=%extra_options% -Dms_speech_sdk5="%programfiles(x86)%/Microsoft Speech SDK 5.1"
+) ELSE (
+  ECHO >&2 Warning: could not locate the Microsoft Speech SDK v5.1; revSpeech will not build
+  SET warnings=1
+)
+
+REM Attempt to locate the Microsoft Speech SDK v4
+IF EXIST "%programfiles(x86)%\Microsoft Speech SDK\*" (
+  SET extra_options=%extra_options% -Dms_speech_sdk4="%programfiles(x86)%/Microsoft Speech SDK"
+) ELSE (
+  ECHO >&2 Warning: could not locate the Microsoft Speech SDK v4; revSpeech will not build
+  SET warnings=1
+)
+
+REM Pause so any warnings can be seen
+IF %warnings% NEQ 0 PAUSE
 
 REM Run the configure step
 %python% gyp\gyp_main.py --format msvs --depth . --generator-output build-win-x86/livecode -Gmsvs_version=2010 %extra_options%
