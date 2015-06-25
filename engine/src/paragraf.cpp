livecode/livecode

commit 0280749eb6bb7ab830b22992508a77ead542bef4

[[Cleanup]] Many, many unhandled enumeration values in switch statements.

engine/src/paragraf.cpp
@@ -2839,6 +2839,10 @@ uint1 MCParagraph::fmovefocus(Field_translations type, bool p_force_logical)
             moving_left = bptr -> is_rtl();
             type = FT_FORWARDWORD;
             break;
+
+		default:
+			MCUnreachable();
+			break;
     }
 
     findex_t oldfocused = focusedindex;

engine/src/eventqueue.cpp
@@ -234,6 +234,8 @@ void MCEventQueueFinalize(void)
 		case kMCEventTypeNotify:
 			t_event -> notify . callback(t_event -> notify . state, false);
 			break;
+		default:
+			break;
 		}
 
 		if (t_event -> type == kMCEventTypeImeCompose)

engine/src/exec-interface-object.cpp
@@ -3812,6 +3812,9 @@ void MCObject::SetRectPoint(MCExecContext& ctxt, bool effective, Properties whic
 	case P_TOP_RIGHT:
 		point . x -= t_rect . width;
 		break;
+	default:
+		MCUnreachable();
+		break;
 	}
 
 	t_rect . x = point . x;
@@ -3878,6 +3881,9 @@ void MCObject::SetRectValue(MCExecContext& ctxt, bool effective, Properties whic
 		if (!getflag(F_LOCK_LOCATION))
 			t_rect . y += (t_rect . height - value) >> 1;
 		t_rect . height = MCU_max(value, 1);
+		break;
+	default:
+		MCUnreachable();
 		break;
 	}
 

engine/src/exec-interface.cpp
@@ -2390,6 +2390,9 @@ void MCInterfaceExecSelectTextOfField(MCExecContext& ctxt, Preposition_type p_ty
 	case PT_AFTER:
 		t_start = t_finish;
 		break;
+	default:
+		MCUnreachable();
+		break;
 	}
     
 	static_cast<MCField *>(p_target . object) -> seltext(t_start, t_finish, True);

engine/src/exec-math.cpp
@@ -752,6 +752,9 @@ void MCMathArrayApplyOperationWithNumber(MCExecContext& ctxt, MCArrayRef p_array
 		case O_OVER:
 			MCMathEvalOver(ctxt, t_number, p_number, t_number);
 			break;
+		default:
+			MCUnreachable();
+			break;
 		}
 		if (ctxt.HasError())
 			return;
@@ -828,6 +831,9 @@ void MCMathArrayApplyOperationWithArray(MCExecContext& ctxt,
 			break;
 		case O_OVER:
 			MCMathEvalOver(ctxt, t_double_left, t_double_right, t_double_left);
+			break;
+		default:
+			MCUnreachable();
 			break;
 		}
 		if (ctxt.HasError())

engine/src/fieldhtml.cpp
@@ -1720,6 +1720,8 @@ static void import_html_change_style(import_html_t& ctxt, const import_html_tag_
 							}
 						}
 						break;
+					default:
+						break;
 					}
 				}
 			}
@@ -1789,6 +1791,8 @@ static void import_html_change_style(import_html_t& ctxt, const import_html_tag_
 			break;
 		case kImportHtmlTagThreeDBox:
 			import_html_add_textstyle_to_style(t_style, FA_3D_BOX);
+			break;
+		default:
 			break;
 	}
 	
@@ -1930,6 +1934,8 @@ static void import_html_parse_paragraph_attrs(import_html_tag_t& p_tag, MCFieldP
 			case kImportHtmlAttrHidden:
 				r_style . hidden = true;
 			break;
+			default:
+				break;
 		}
 	}
 }
@@ -2206,6 +2212,9 @@ MCParagraph *MCField::importhtmltext(MCValueRef p_text)
                                 case kImportHtmlTagH6:
                                     t_font_size = 10, t_font_style = FA_BOLD;
                                     break;
+								default:
+									MCUnreachable();
+									break;
                             }
                             
                             if (t_font_style != 0)
@@ -2270,6 +2279,8 @@ MCParagraph *MCField::importhtmltext(MCValueRef p_text)
                         else
                             import_html_change_style(ctxt, t_tag);
                         break;
+					default:
+						break;
 				}
                 
 				t_saw_start_tag = !t_tag . is_terminator;

engine/src/funcs.cpp
@@ -1455,6 +1455,9 @@ void MCChunkOffset::eval_ctxt(MCExecContext &ctxt, MCExecValue &r_value)
     case CT_CODEUNIT:
         MCStringsEvalCodeunitOffset(ctxt, *t_chunk, *t_string, t_start, r_value . uint_value);
         break;
+	default:
+		MCUnreachable();
+		break;
 	}
     
     r_value . type = kMCExecValueTypeUInt;

engine/src/lnxdcs.cpp
@@ -1220,6 +1220,10 @@ MCImageBitmap *MCScreenDC::snapshot(MCRectangle &r, uint4 window, MCStringRef di
                 case GDK_GRAB_BROKEN:
                     t_done = true;
                     break;
+
+				default:
+					/* Ignore this event */
+					break;
             }
             
             // The event needs to be released

commit 0a1a2bc27d1e800954c811ede77a44ad3b58b058

[Bug 15549] MCParagraph::fmovefocus(): Allow FT_UNDEFINED.
Regression introduced in commit 0280749eb6bb.

engine/src/paragraf.cpp
@@ -2840,6 +2840,9 @@ uint1 MCParagraph::fmovefocus(Field_translations type, bool p_force_logical)
             type = FT_FORWARDWORD;
             break;
 
+		case FT_UNDEFINED:
+			break;
+
 		default:
 			MCUnreachable();
 			break;