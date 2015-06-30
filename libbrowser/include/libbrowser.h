d869584da34c720cc3f465f5b9751018b7ff0eb6
Ian Macphail <ian.macphail@livecode.com>
2015-06-30 16:03:42 +0100

    [[ BrowserWidget ]] Add export tags to C API functions

--- libbrowser/include/libbrowser.h
+++ libbrowser/include/libbrowser.h
@@ -136,19 +136,33 @@ public:
 	virtual bool CreateBrowser(MCBrowser *&r_browser) = 0;
 };
 
+////////////////////////////////////////////////////////////////////////////////
+//
+//  SYMBOL EXPORTS
+//
+#ifdef _WIN32
+#  ifdef _MSC_VER
+#    define MC_DLLEXPORT __declspec(dllexport)
+#  else
+#    define MC_DLLEXPORT __attribute__((dllexport))
+#  endif
+#else
+#  define MC_DLLEXPORT __attribute__((__visibility__("default")))
+#endif
+
 // C API
 
 extern "C"
 {
 	
-bool MCBrowserLibraryInitialize();
-void MCBrowserLibraryFinalize();
+MC_DLLEXPORT bool MCBrowserLibraryInitialize();
+MC_DLLEXPORT void MCBrowserLibraryFinalize();
 	
-	typedef void *(MCBrowserAllocator)(size_t p_size);
-	typedef void (MCBrowserDeallocator)(void *p_mem);
+typedef void *(MCBrowserAllocator)(size_t p_size);
+typedef void (MCBrowserDeallocator)(void *p_mem);
 	
-	void MCBrowserLibrarySetAllocator(MCBrowserAllocator p_alloc);
-	void MCBrowserSetDeallocator(MCBrowserDeallocator p_dealloc);
+MC_DLLEXPORT void MCBrowserLibrarySetAllocator(MCBrowserAllocator p_alloc);
+MC_DLLEXPORT void MCBrowserLibrarySetDeallocator(MCBrowserDeallocator p_dealloc);
 
 typedef struct __MCBrowserList *MCBrowserListRef;
 enum MCBrowserValueType
@@ -161,52 +175,52 @@ enum MCBrowserValueType
 	kMCBrowserValueTypeList,
 };
 	
-bool MCBrowserListCreate(MCBrowserListRef &r_browser, uint32_t p_size = 0);
-MCBrowserListRef MCBrowserListRetain(MCBrowserListRef p_list);
-void MCBrowserListRelease(MCBrowserListRef p_list);
+MC_DLLEXPORT bool MCBrowserListCreate(MCBrowserListRef &r_browser, uint32_t p_size = 0);
+MC_DLLEXPORT MCBrowserListRef MCBrowserListRetain(MCBrowserListRef p_list);
+MC_DLLEXPORT void MCBrowserListRelease(MCBrowserListRef p_list);
 	
-bool MCBrowserListGetSize(MCBrowserListRef p_list, uint32_t &r_size);
-bool MCBrowserListGetType(MCBrowserListRef p_list, uint32_t p_index, MCBrowserValueType &r_type);
+MC_DLLEXPORT bool MCBrowserListGetSize(MCBrowserListRef p_list, uint32_t &r_size);
+MC_DLLEXPORT bool MCBrowserListGetType(MCBrowserListRef p_list, uint32_t p_index, MCBrowserValueType &r_type);
 	
-bool MCBrowserListSetBoolean(MCBrowserListRef p_list, uint32_t p_index, bool p_value);
-bool MCBrowserListSetInteger(MCBrowserListRef p_list, uint32_t p_index, int32_t p_value);
-bool MCBrowserListSetDouble(MCBrowserListRef p_list, uint32_t p_index, double_t p_value);
-bool MCBrowserListSetUTF8String(MCBrowserListRef p_list, uint32_t p_index, const char *p_value);
-bool MCBrowserListSetList(MCBrowserListRef p_list, uint32_t p_index, MCBrowserListRef p_value);
+MC_DLLEXPORT bool MCBrowserListSetBoolean(MCBrowserListRef p_list, uint32_t p_index, bool p_value);
+MC_DLLEXPORT bool MCBrowserListSetInteger(MCBrowserListRef p_list, uint32_t p_index, int32_t p_value);
+MC_DLLEXPORT bool MCBrowserListSetDouble(MCBrowserListRef p_list, uint32_t p_index, double_t p_value);
+MC_DLLEXPORT bool MCBrowserListSetUTF8String(MCBrowserListRef p_list, uint32_t p_index, const char *p_value);
+MC_DLLEXPORT bool MCBrowserListSetList(MCBrowserListRef p_list, uint32_t p_index, MCBrowserListRef p_value);
 	
-bool MCBrowserListAppendBoolean(MCBrowserListRef p_list, bool p_value);
-bool MCBrowserListAppendInteger(MCBrowserListRef p_list, int32_t p_value);
-bool MCBrowserListAppendDouble(MCBrowserListRef p_list, double_t p_value);
-bool MCBrowserListAppendUTF8String(MCBrowserListRef p_list, const char *p_value);
-bool MCBrowserListAppendList(MCBrowserListRef p_list, MCBrowserListRef p_value);
+MC_DLLEXPORT bool MCBrowserListAppendBoolean(MCBrowserListRef p_list, bool p_value);
+MC_DLLEXPORT bool MCBrowserListAppendInteger(MCBrowserListRef p_list, int32_t p_value);
+MC_DLLEXPORT bool MCBrowserListAppendDouble(MCBrowserListRef p_list, double_t p_value);
+MC_DLLEXPORT bool MCBrowserListAppendUTF8String(MCBrowserListRef p_list, const char *p_value);
+MC_DLLEXPORT bool MCBrowserListAppendList(MCBrowserListRef p_list, MCBrowserListRef p_value);
 	
-bool MCBrowserListGetBoolean(MCBrowserListRef p_list, uint32_t p_index, bool &r_value);
-bool MCBrowserListGetInteger(MCBrowserListRef p_list, uint32_t p_index, int32_t &r_value);
-bool MCBrowserListGetDouble(MCBrowserListRef p_list, uint32_t p_index, double_t &r_value);
-bool MCBrowserListGetUTF8String(MCBrowserListRef p_list, uint32_t p_index, char *&r_value);
-bool MCBrowserListGetList(MCBrowserListRef p_list, uint32_t p_index, MCBrowserListRef &r_value);
+MC_DLLEXPORT bool MCBrowserListGetBoolean(MCBrowserListRef p_list, uint32_t p_index, bool &r_value);
+MC_DLLEXPORT bool MCBrowserListGetInteger(MCBrowserListRef p_list, uint32_t p_index, int32_t &r_value);
+MC_DLLEXPORT bool MCBrowserListGetDouble(MCBrowserListRef p_list, uint32_t p_index, double_t &r_value);
+MC_DLLEXPORT bool MCBrowserListGetUTF8String(MCBrowserListRef p_list, uint32_t p_index, char *&r_value);
+MC_DLLEXPORT bool MCBrowserListGetList(MCBrowserListRef p_list, uint32_t p_index, MCBrowserListRef &r_value);
 	
 typedef struct __MCBrowser *MCBrowserRef;
 typedef struct __MCBrowserFactory *MCBrowserFactoryRef;
 
-bool MCBrowserFactoryGet(const char *p_factory_id, MCBrowserFactoryRef &r_factory);
-bool MCBrowserFactoryCreateBrowser(MCBrowserFactoryRef p_factory, MCBrowserRef &r_browser);
+MC_DLLEXPORT bool MCBrowserFactoryGet(const char *p_factory_id, MCBrowserFactoryRef &r_factory);
+MC_DLLEXPORT bool MCBrowserFactoryCreateBrowser(MCBrowserFactoryRef p_factory, MCBrowserRef &r_browser);
 
-MCBrowserRef MCBrowserRetain(MCBrowserRef p_browser);
-void MCBrowserRelease(MCBrowserRef p_browser);
+MC_DLLEXPORT MCBrowserRef MCBrowserRetain(MCBrowserRef p_browser);
+MC_DLLEXPORT void MCBrowserRelease(MCBrowserRef p_browser);
 
-void *MCBrowserGetNativeLayer(MCBrowserRef p_browser);
+MC_DLLEXPORT void *MCBrowserGetNativeLayer(MCBrowserRef p_browser);
 
-bool MCBrowserGetBoolProperty(MCBrowserRef p_browser, MCBrowserProperty p_property, bool &r_value);
-bool MCBrowserSetBoolProperty(MCBrowserRef p_browser, MCBrowserProperty p_property, bool p_value);
+MC_DLLEXPORT bool MCBrowserGetBoolProperty(MCBrowserRef p_browser, MCBrowserProperty p_property, bool &r_value);
+MC_DLLEXPORT bool MCBrowserSetBoolProperty(MCBrowserRef p_browser, MCBrowserProperty p_property, bool p_value);
 
-bool MCBrowserGetStringProperty(MCBrowserRef p_browser, MCBrowserProperty p_property, char *&r_utf8_string);
-bool MCBrowserSetStringProperty(MCBrowserRef p_browser, MCBrowserProperty p_property, const char *p_utf8_string);
+MC_DLLEXPORT bool MCBrowserGetStringProperty(MCBrowserRef p_browser, MCBrowserProperty p_property, char *&r_utf8_string);
+MC_DLLEXPORT bool MCBrowserSetStringProperty(MCBrowserRef p_browser, MCBrowserProperty p_property, const char *p_utf8_string);
 
-bool MCBrowserGoBack(MCBrowserRef p_browser);
-bool MCBrowserGoForward(MCBrowserRef p_browser);
-bool MCBrowserGoToURL(MCBrowserRef p_browser);
-bool MCBrowserEvaluateJavaScript(MCBrowserRef p_browser, const char *p_script, char *&r_result);
+MC_DLLEXPORT bool MCBrowserGoBack(MCBrowserRef p_browser);
+MC_DLLEXPORT bool MCBrowserGoForward(MCBrowserRef p_browser);
+MC_DLLEXPORT bool MCBrowserGoToURL(MCBrowserRef p_browser);
+MC_DLLEXPORT bool MCBrowserEvaluateJavaScript(MCBrowserRef p_browser, const char *p_script, char *&r_result);
 
 enum MCBrowserRequestType
 {
@@ -224,8 +238,8 @@ enum MCBrowserRequestState
 typedef void (*MCBrowserRequestCallback)(void *p_context, MCBrowserRef p_browser, MCBrowserRequestType p_type, MCBrowserRequestState p_state, bool p_in_frame, const char *p_url, const char *p_error);
 typedef void (*MCBrowserJavaScriptCallback)(void *p_context, MCBrowserRef p_browser, const char *p_handler, MCBrowserListRef p_params);
 
-bool MCBrowserSetRequestHandler(MCBrowserRef p_browser, MCBrowserRequestCallback p_callback, void *p_context);
-bool MCBrowserSetJavaScriptHandler(MCBrowserRef p_browser, MCBrowserJavaScriptCallback p_callback, void *p_context);
+MC_DLLEXPORT bool MCBrowserSetRequestHandler(MCBrowserRef p_browser, MCBrowserRequestCallback p_callback, void *p_context);
+MC_DLLEXPORT bool MCBrowserSetJavaScriptHandler(MCBrowserRef p_browser, MCBrowserJavaScriptCallback p_callback, void *p_context);
 
 }
 
