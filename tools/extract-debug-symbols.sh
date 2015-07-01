8d713c92bc069aa705315f4f70bb68908d38eb49
Fraser J. Gordon <fraser.gordon@livecode.com>
2015-07-01 15:03:17 +0100

    Add a no-op extract-debug-symbols step for emscripten

--- tools/extract-debug-symbols.sh
+++ tools/extract-debug-symbols.sh
@@ -34,6 +34,14 @@ done
 
 }
 
+function extract_emscripten {
+
+for input in $@ ; do
+	touch "${input}${suffix}"
+done
+
+}
+
 function extract_mac_or_ios {
 
 for input in $@ ; do
@@ -69,6 +77,9 @@ case $os in
 	mac|ios)
 		extract_mac_or_ios ${inputs}
 		;;
+	emscripten)
+		extract_emscripten ${inputs}
+		;;	
 	*)
 		echo OS "$os" not supported by this script
 		exit 1
