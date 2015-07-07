3bea85c9b0d208f12ff079b191388aef235872c8
Peter TB Brett <peter.brett@livecode.com>
2015-07-07 10:22:30 +0100

    [emscripten] Prevent recursive calls to LiveCodeAsync.resume().

--- engine/src/em-async.js
+++ engine/src/em-async.js
@@ -60,9 +60,13 @@ mergeInto(LibraryManager.library, {
 		resume: function() {
 			LiveCodeAsync._ensureInit();
 
+			// Don't allow recursive calls to resume()
+			if (LiveCodeAsync._inPreResume) {
+				return;
+			}
+
 			// Make sure we're actually currently in a yield state.
 			assert(LiveCodeAsync._continuation);
-			assert(!LiveCodeAsync._inPreResume);
 
 			// Cancel the timeout for this yield state
 			if (LiveCodeAsync._timeoutHandle) {
