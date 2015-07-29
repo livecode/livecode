db24e4d9d35cb8a3c094b455a6a85e9b34c43aee
Sebastien Nouat <sebastien.nouat@livecode.com>
2015-07-29 17:10:31 +0100

    [[ Bug 15649 ]] Allow the cursor property to return an empty value

--- engine/src/exec.h
+++ engine/src/exec.h
@@ -3202,8 +3202,9 @@ void MCInterfaceSetProcessType(MCExecContext& ctxt, intenum_t value);
 void MCInterfaceGetShowInvisibles(MCExecContext& ctxt, bool& r_value);
 void MCInterfaceSetShowInvisibles(MCExecContext& ctxt, bool p_value);
 
-void MCInterfaceGetCursor(MCExecContext& ctxt, uinteger_t& r_value);
-void MCInterfaceSetCursor(MCExecContext& ctxt, uinteger_t p_value);
+// SN-2015-07-29: [[ Bug 15649 ]] The cursor can be empty - it is optional
+void MCInterfaceGetCursor(MCExecContext& ctxt, uinteger_t *&r_value);
+void MCInterfaceSetCursor(MCExecContext& ctxt, uinteger_t* p_value);
 void MCInterfaceGetDefaultCursor(MCExecContext& ctxt, uinteger_t& r_value);
 void MCInterfaceSetDefaultCursor(MCExecContext& ctxt, uinteger_t p_value);
 void MCInterfaceGetDefaultStack(MCExecContext& ctxt, MCStringRef& r_value);
