livecode/livecode

commit 39eb28e112649503aa922aea3543ae0e9315a62b

[env-vars] Add MCU_is_token().
Add a utility function that checks whether a given string is a valid
LiveCode script token.

engine/src/util.cpp
@@ -46,6 +46,7 @@ along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
 #include "exec.h"
 #include "system.h"
 #include "dispatch.h"
+#include "scriptpt.h"
 
 #if defined(_MACOSX)
 #include <mach-o/dyld.h>
@@ -3344,6 +3345,37 @@ void *MCU_resolvemodulesymbol(void* p_module, const char *p_symbol)
         return nil;
 
     return MCS_resolvemodulesymbol((MCSysModuleHandle)p_module, *t_symbol_str);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+bool
+MCU_is_token(MCStringRef p_string)
+{
+	MCScriptPoint sp(p_string);
+
+	MCerrorlock++;
+
+	Parse_stat ps = sp.nexttoken();
+
+	if (ps == PS_ERROR || ps == PS_EOF)
+	{
+		return false;
+	}
+
+	/* Check that token is located at start of query string */
+	if (sp.getindex() != 0)
+	{
+		return false;
+	}
+
+	/* Check that token spans full length of query string */
+	if (MCStringGetLength(p_string) != MCStringGetLength(sp.gettoken_stringref()))
+	{
+		return false;
+	}
+
+	return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////

engine/src/util.h
@@ -288,4 +288,7 @@ inline MCRectangle MCU_make_rect(int2 x, int2 y, uint2 w, uint2 h)
 	return r;
 }
 
+// Test whether p_string is a valid LiveCode script token
+extern bool MCU_is_token(MCStringRef p_string);
+
 #endif

commit 2423fd8ff091b6444ada07eeb5ff4d77b702ba38

[env-vars] MCU_is_token(): Clear MCerrorlock.
Bug introduced in commit 39eb28e.

engine/src/util.cpp
@@ -3354,9 +3354,11 @@ MCU_is_token(MCStringRef p_string)
 {
 	MCScriptPoint sp(p_string);
 
-	MCerrorlock++;
+	++MCerrorlock;
 
 	Parse_stat ps = sp.nexttoken();
+
+	--MCerrorlock;
 
 	if (ps == PS_ERROR || ps == PS_EOF)
 	{