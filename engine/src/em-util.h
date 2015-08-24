eae1d3dd96cfa22f2427d01207040b2e4083f4ac
Peter TB Brett <peter.brett@livecode.com>
2015-08-24 14:23:31 +0100

    [emscripten] Correct fix for MCEmscriptenNotImplemented()
    Make `MCEmscriptenNotImplemented()` be a no-op in non-debug builds.

--- engine/src/em-util.h
+++ engine/src/em-util.h
@@ -30,7 +30,9 @@ __MCEmscriptenNotImplemented(const char *p_file,
                              uint32_t p_line,
                              const char *p_function)
 {
+#if defined(_DEBUG)
 	__MCLog(p_file, p_line, "not implemented: %s", p_function);
+#endif /* _DEBUG */
 }
 
 #define MCEmscriptenNotImplemented() __MCEmscriptenNotImplemented(__FILE__, __LINE__, __PRETTY_FUNCTION__)
