1afc8c45cb5a014d8f414fbe5dcf22bb07e8950e
Peter TB Brett <peter.brett@livecode.com>
2015-09-22 21:35:31 +0100

    libfoundation: Make sure MCHandlerTypeLayout.abi is initialised.

--- libfoundation/src/foundation-typeinfo.cpp
+++ libfoundation/src/foundation-typeinfo.cpp
@@ -926,6 +926,8 @@ bool MCHandlerTypeInfoGetLayoutType(MCTypeInfoRef unresolved_self, int p_abi, vo
     MCHandlerTypeLayout *t_layout;
     if (!MCMemoryAllocate(sizeof(MCHandlerTypeLayout) + sizeof(ffi_cif), t_layout))
         return false;
+
+	t_layout -> abi = p_abi;
     
     if (ffi_prep_cif((ffi_cif *)&t_layout -> cif, (ffi_abi)p_abi, self -> handler . field_count, (ffi_type *)self -> handler . layout_args[0], (ffi_type **)(self -> handler . layout_args + 1)) != FFI_OK)
     {
