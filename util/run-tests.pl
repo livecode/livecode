463801f2a3f1f1380936a361c7a4df3ddb8eb2dc
Stephen Tawn <stephen.tawn@livecode.com>
2015-10-02 10:40:03 +0100

    [[ cpptest ]] Correct failure to catch invalid test wrapper

--- util/run-tests.pl
+++ util/run-tests.pl
@@ -11,4 +11,4 @@ $test_command_line = "$test_wrapper $test_exec --tap=$test_log";
 
 chdir $test_dir or die "Failed to change directory!";
 print "$test_command_line\n";
-exec "$test_command_line" || die "Failed to run tests!";
+exec "$test_command_line" or die "Failed to run tests!";
