livecode/livecode

commit 324bef8a916ca9ac5d18ba84ef8d88cb8a82ff4f

libfoundation: Correct local variable naming in MCStringCreateWithSysString()
Locals should use `t_`, not `p_`.

libfoundation/src/foundation-string.cpp
@@ -5449,11 +5449,11 @@ MCStringCreateWithSysString(const char *p_sys_string,
 	}
 
 	/* Count the number of chars */
-	size_t p_byte_count;
-	for (p_byte_count = 0; p_sys_string[p_byte_count] != '\0'; ++p_byte_count);
+	size_t t_byte_count;
+	for (t_byte_count = 0; p_sys_string[p_byte_count] != '\0'; ++t_byte_count);
 
 	return MCStringCreateWithBytes((const byte_t *) p_sys_string,
-	                               p_byte_count,
+	                               t_byte_count,
 	                               kMCStringEncodingUTF8,
 	                               false, /* is_external_rep */
 	                               r_string);

commit ba7a991dd19be8ec7ac6f348f1c5227943fe9531

libfoundation: Correct variable misnaming in MCStringCreateWithSysString().
Error introduced in commit  324bef8a916c.

libfoundation/src/foundation-string.cpp
@@ -5450,7 +5450,7 @@ MCStringCreateWithSysString(const char *p_sys_string,
 
 	/* Count the number of chars */
 	size_t t_byte_count;
-	for (t_byte_count = 0; p_sys_string[p_byte_count] != '\0'; ++t_byte_count);
+	for (t_byte_count = 0; p_sys_string[t_byte_count] != '\0'; ++t_byte_count);
 
 	return MCStringCreateWithBytes((const byte_t *) p_sys_string,
 	                               t_byte_count,