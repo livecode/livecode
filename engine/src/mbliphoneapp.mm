e1cf21011834f924feeaa4ce324e6a876393a330
runrevpanos <panos.merakos@runrev.com>
2015-11-05 16:00:46 +0000

    [Bug 16279] Better handling of NSNull returned by objectForKey:

--- engine/src/mbliphoneapp.mm
+++ engine/src/mbliphoneapp.mm
@@ -364,14 +364,18 @@ static UIDeviceOrientation patch_device_orientation(id self, SEL _cmd)
     NSDictionary *t_push_notification = [p_launchOptions objectForKey: UIApplicationLaunchOptionsRemoteNotificationKey];
     // MM-2012-09-25: [[ Bug 10391 ]] Retain notification.
     if (t_push_notification)
-        m_pending_push_notification = [[t_push_notification objectForKey:@"payload"] retain];
+	{
+		// Prevent crash when launching the app from the notifications screen:
+		// If the payload is empty, JSON null will be deserialized to NSNull. Unlike nil,
+		// we cannot send (most) messages to NSNull
+		id t_pending_push_notification_value = [[t_push_notification objectForKey:@"payload"] retain];
+		if ([t_pending_push_notification_value isKindOfClass: [NSString class]])
+			m_pending_push_notification = t_pending_push_notification_value;
+		else
+			// t_pending_push_notification_value is NSNull, not NSString
+			m_pending_push_notification = nil;
+	}
 	
-	// Prevent crash when launching the app from the notifications screen:
-	// JSON null will be deserialized to NSNull. Unlike nil, we cannot send (most) messages to NSNull, so
-	// in this case, convert NSNull to nil
-	if ([m_pending_push_notification isKindOfClass:[NSNull class]])
-		m_pending_push_notification = nil;
-
     // Check if we have received a custom URL
     // This check is carried out at application launch
     NSURL *t_launch_url = [p_launchOptions objectForKey: UIApplicationLaunchOptionsURLKey];
@@ -519,13 +523,18 @@ static UIDeviceOrientation patch_device_orientation(id self, SEL _cmd)
     MCLog("application:didReceiveRemoteNotification:");
     UIApplicationState t_state = [p_application applicationState];
     MCAutoStringRef t_mc_push_notification_text;
-    NSString *t_reminder_text = [p_dictionary objectForKey:@"payload"];
+	id t_reminder_text_value = [p_dictionary objectForKey:@"payload"];
 	
 	// Prevent crash when sending push notifications while the app is running:
-	// JSON null will be deserialized to NSNull. Unlike nil, we cannot send (most) messages to NSNull, so
-	// in this case, convert NSNull to nil
-	if ([t_reminder_text isKindOfClass:[NSNull class]])
+	// If the payload is empty, JSON null will be deserialized to NSNull. Unlike nil,
+	// we cannot send (most) messages to NSNull
+	NSString *t_reminder_text;
+	if ([t_reminder_text_value isKindOfClass: [NSString class]])
+		t_reminder_text = t_reminder_text_value;
+	else
+		// t_reminder_text_value is NSNull, not NSString
 		t_reminder_text = nil;
+	
 	
 	// PM-2015-10-27: [[ Bug 16279 ]] Prevent crash when the payload is empty
 	if (t_reminder_text != nil)
