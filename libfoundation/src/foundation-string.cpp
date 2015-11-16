9493387789e29b08a467dfb456201198bddc1eb1
Peter TB Brett <peter.brett@livecode.com>
2015-11-16 16:14:55 +0000

    libfoundation: MCStringFind() should never locate empty
    `MCStringFind()` should never succeed in searching for an empty
    needle.

--- libfoundation/src/foundation-string.cpp
+++ libfoundation/src/foundation-string.cpp
@@ -3250,7 +3250,11 @@ bool MCStringFind(MCStringRef self, MCRange p_range, MCStringRef p_needle, MCStr
     
     // Circumvent performance hit due to possibility of case / form sensitivity affecting delimiter search.
     // TODO: Implement properly, based on properties of the needle string.
-    if (__MCStringGetLength(p_needle) == 1)
+    if (__MCStringGetLength(p_needle) == 0)
+    {
+        return false;
+    }
+    else if (__MCStringGetLength(p_needle) == 1)
     {
         codepoint_t t_codepoint =  MCStringGetCodepointAtIndex(p_needle, 0);
         // if codepoint is among first 64 ASCII characters then do case and form sensitive comparison.
