06a4fe1197b362099c2745405b35d296411ac7a4
sebastien <sebastien.nouat@livecode.com>
2015-11-18 15:01:57 +0000

    [[ Build ]] Fix release note upload - only from linux builder

--- buildbot.mk
+++ buildbot.mk
@@ -100,10 +100,12 @@ ifeq ($(BUILD_PLATFORM),mac)
   LIVECODE = $(bin_dir)/LiveCode-Community.app/Contents/MacOS/LiveCode-Community
   buildtool_platform = mac
   UPLOAD_ENABLE_CHECKSUM ?= no
+  UPLOAD_RELEASE_NOTES ?= no
 else ifeq ($(findstring linux,$(BUILD_PLATFORM)),linux)
   LIVECODE = $(bin_dir)/livecode-community
   buildtool_platform = linux
   UPLOAD_ENABLE_CHECKSUM ?= yes
+  UPLOAD_RELEASE_NOTES ?= yes
 endif
 
 # FIXME add --warn-as-error
@@ -155,6 +157,7 @@ dist-tools-commercial:
 
 # Make a list of installers to be uploaded to the distribution server, and release notes
 # If a checksum file is needed, generate it with sha1sum
+# Upload the release notes if we are on Linux
 dist-upload-files.txt sha1sum.txt:
 	set -e; \
 	find . -maxdepth 1 -name 'LiveCode*Installer-*-Mac.dmg' \
@@ -165,7 +168,9 @@ dist-upload-files.txt sha1sum.txt:
 	                -o -name 'LiveCode*Server-*-Windows.zip' \
 	                -o -name '*-bin.tar.xz' \
 	  > dist-upload-files.txt; \
-	find "${notes_dir}" -name 'LiveCodeNotes*.pdf' >> dist-upload-files.txt; \
+	if test "${UPLOAD_RELEASE_NOTES}" = "yes"; then \
+		find "${notes_dir}" -name 'LiveCodeNotes*.pdf' >> dist-upload-files.txt; \
+	fi \
 	if test "$(UPLOAD_ENABLE_CHECKSUM)" = "yes"; then \
 	  $(SHA1SUM) < dist-upload-files.txt > sha1sum.txt; \
 	  echo sha1sum.txt >> dist-upload-files.txt; \
