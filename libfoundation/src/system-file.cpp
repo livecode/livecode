e03cf019b7b4aed78c0458621652ef72ea49e9b5
Peter TB Brett <peter.brett@livecode.com>
2015-11-19 15:10:58 +0000

    libsystem: Add API for retrieving CWD.
    Add `MCSFileGetCurrentDirectory()` for retrieving the process's
    current working directory.

--- libfoundation/src/system-file.cpp
+++ libfoundation/src/system-file.cpp
@@ -104,6 +104,24 @@ __MCSFileThrowInvalidPathError (MCStringRef p_path)
 }
 
 /* ================================================================
+ * Path manipulation
+ * ================================================================ */
+
+MC_DLLEXPORT_DEF bool
+MCSFileGetCurrentDirectory (MCStringRef & r_path)
+{
+	MCAutoStringRef t_native_path;
+
+	if (!__MCSFileGetCurrentDirectory (&t_native_path))
+		return false;
+
+	if (!__MCSFilePathFromNative(*t_native_path, r_path))
+		return false;
+
+	return true;
+}
+
+/* ================================================================
  * Whole-file IO
  * ================================================================ */
 
