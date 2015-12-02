2f66548242f7a0f532590484854fbc87d3ce2bdd
Sébastien Nouat <sebastien.nouat@livecode.com>
2015-12-02 09:32:53 +0000

    [[ Bug 16528 ]] Fix commandArguments/commandName crash in IDE
    Sets an empty value to MCcommandarguments / MCcommandname if we are not in a mode where we allow them to be populated

--- engine/src/globals.cpp
+++ engine/src/globals.cpp
@@ -1114,6 +1114,11 @@ bool X_open(int argc, MCStringRef argv[], MCStringRef envp[])
         if (!t_success)
             return false;
     }
+    else
+    {
+        MCcommandname = MCValueRetain(kMCEmptyString);
+        MCcommandarguments = MCValueRetain(kMCEmptyArray);
+    }
     
     MCDeletedObjectsSetup();
 
