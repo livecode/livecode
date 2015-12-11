f4939bab50e23105acd4efb71feb55daad581322
livecodeali <ali.lloyd@gmail.com>
2015-12-11 11:07:32 +0000

    [[ Bug 16584 ]] Ensure source and pattern marks are in sync when wildcard matching

--- libfoundation/src/foundation-unicode.cpp
+++ libfoundation/src/foundation-unicode.cpp
@@ -2322,8 +2322,6 @@ bool MCUnicodeWildcardMatch(const void *source_chars, uindex_t source_length, bo
                 // try and match the rest of the source string recursively.
                 while (t_source_filter -> HasData())
                 {
-                    t_source_cp = t_source_filter -> GetNextCodepoint();
-                    
                     uindex_t t_sindex, t_pindex;
                     // if this is a candidate for a match, recurse.
                     if (t_source_cp == t_pattern_cp)
@@ -2355,6 +2353,7 @@ bool MCUnicodeWildcardMatch(const void *source_chars, uindex_t source_length, bo
                     
                     // if we don't find a match, eat the source codepoint and continue.
                     t_source_filter -> AdvanceCursor();
+                    t_source_cp = t_source_filter -> GetNextCodepoint();
                 }
             }
                 return false;
