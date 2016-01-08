747275b2eed6edc5ce5577d52373d5e6567373c1
runrevpanos <panos.merakos@runrev.com>
2016-01-08 16:28:18 +0000

    [Bug 16666] Get flaggedRanges with the correct offset

--- engine/src/exec-interface-field-chunk.cpp
+++ engine/src/exec-interface-field-chunk.cpp
@@ -1820,6 +1820,8 @@ void MCField::GetFlaggedRangesOfCharChunk(MCExecContext& ctxt, uint32_t p_part_i
     {
         MCInterfaceFieldRanges t_paragraphRanges;
         sptr -> getflaggedranges(p_part_id, si, ei, t_index_offset, t_paragraphRanges);
+		// PM-2016-01-08: [[ Bug 16666 ]] Update the offset to be relative to the beginning of the text
+		t_index_offset += sptr -> gettextlengthcr();
 
         for (uindex_t i = 0; i < t_paragraphRanges . count; ++i)
             t_ranges . Push(t_paragraphRanges . ranges[i]);
