bf4f93575b59c97a9f389e534d823d41b1cdbb43
Charles Warwick <charles@techstrategies.com.au>
2016-04-04 14:39:00 +1000

    Always build dylib form of the external

--- lcidlc/lclink.sh
+++ lcidlc/lclink.sh
@@ -106,9 +106,9 @@ if [ -z "$FAT_INFO" -o $BUILD_DYLIB -eq 1 ]; then
 
     ARCHS="-arch ${ARCHS// / -arch }"
 
-	if [ $BUILD_DYLIB -eq 1 ]; then
-		"$DEVELOPER_BIN_DIR/g++" -stdlib=libc++ -dynamiclib $ARCHS $MIN_OS_VERSION -isysroot "$SDKROOT" $FRAMEWORK_SEARCH_PATHS $STATIC_FRAMEWORKS -o "$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.dylib" "$BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME" $SYMBOL_ARGS $SYMBOLS $DEPS $OTHER_FLAGS
-	fi
+	# Build the 'dylib' form of the external - this is used by simulator builds, and as
+	# a dependency check for device builds - otherwise dsymutil call below may fail
+	"$DEVELOPER_BIN_DIR/g++" -stdlib=libc++ -dynamiclib $ARCHS $MIN_OS_VERSION -isysroot "$SDKROOT" $FRAMEWORK_SEARCH_PATHS $STATIC_FRAMEWORKS -o "$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.dylib" "$BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME" $SYMBOL_ARGS $SYMBOLS $DEPS $OTHER_FLAGS
 
 	if [ $? -ne 0 ]; then
 		exit $?
