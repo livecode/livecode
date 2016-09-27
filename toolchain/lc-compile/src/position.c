livecode/livecode

commit 6c44baa934efb59b0e1b4e6e4ac85a314a3f09dd

[Bug 18463] lc-compile: Show correct error position for tab-indented lines
lc-compile should show the correct source position when displaying
tab-indented lines.

This patch modifies lc-compile to sanitise ASCII control characters
from lines of LCB source code before printing them.  This has two
benefits:

- Compiling LCB code will never screw up your terminal emulator
- The caret that indicates the position of an error will appear in the
  right place.

toolchain/lc-compile/src/report.c
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 extern int IsDependencyCompile(void);
 
@@ -174,15 +175,31 @@ static void _PrintPosition(long p_position)
  * that the position specifies. */
 static void _PrintContext(long p_position)
 {
-	const char *t_text = NULL;
+	const char *t_raw_text = NULL;
+	char *t_text = NULL;
 	long t_column;
-	GetRowTextOfPosition(p_position, &t_text);
+	int i;
+	GetRowTextOfPosition(p_position, &t_raw_text);
 	GetColumnOfPosition(p_position, &t_column);
-	if (NULL != t_text)
+
+	if (NULL == t_raw_text)
+		return;
+
+	/* Replace ASCII control characters in the raw text with spaces.
+	 * This has two goals: it simplifies aligning the context
+	 * indicator caret, and it ensures that control characters in the
+	 * LCB source code can't screw with your terminal emulator.  Note
+	 * that this doesn't try to cope with invalid Unicode values. */
+	t_text = strdup(t_raw_text);
+	if (NULL == t_text)
+		Fatal_OutOfMemory();
+	for (i = 0; t_text[i] != 0; ++i)
 	{
-		fprintf(stderr, " %s\n", t_text);
-		fprintf(stderr, " %*c\n", (int)t_column, '^');
+		if (t_text[i] < 0x20 || t_text[i] == 0x7F)
+			t_text[i] = ' ';
 	}
+
+	fprintf(stderr, " %s\n %*c\n", t_text, (int)t_column, '^');
 }
 
 static void _Error(long p_position, const char *p_message)

commit 62b71b74306f59285474c298195e07ae42cdcafc

lc-compile: Fix Coverity defects found in context display
This patch resolves three defects found by Coverity:

- commit 6c44baa934ef introduced a memory leak when removing control
  characters from source code lines
- commit c462755aff8a introduced a file pointer leak that could occur
  in some circumstances when attempting to print messages for
  unseekable files, and some unreachable cleanup code.

Coverity-Id: 136143
Coverity-Id: 136987
Coverity-Id: 136999

toolchain/lc-compile/src/position.c
@@ -17,6 +17,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "report.h"
 #include "position.h"
@@ -419,6 +420,7 @@ static void __InitializeFileLines(FileRef x_file)
 	t_raw_text[t_file_length] = 0; /* nul-terminate */
 
 	fclose(t_stream);
+	t_stream = NULL;
 
 	/* Scan the file contents twice: once to count the number of
 	 * lines, and once to fill a pointer array with offset
@@ -470,10 +472,11 @@ static void __InitializeFileLines(FileRef x_file)
 	      x_file->path, x_file->line_count);
 
  cleanup:
+	if (NULL != t_stream)
+		fclose(t_stream);
 	if (NULL != t_raw_text)
 		free(t_raw_text);
-	if (NULL != t_lines)
-		free(t_lines);
+	assert(NULL == t_lines);
 	return;
 }
 

toolchain/lc-compile/src/report.c
@@ -200,6 +200,7 @@ static void _PrintContext(long p_position)
 	}
 
 	fprintf(stderr, " %s\n %*c\n", t_text, (int)t_column, '^');
+	free(t_text);
 }
 
 static void _Error(long p_position, const char *p_message)