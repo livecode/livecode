5cc7743b5b5810e67be9b4ec9efe191ba9e49401
Peter TB Brett <peter.brett@livecode.com>
2016-09-29 22:38:33 +0100

    libfoundation: Re-indent system integration library

--- libfoundation/src/system-stream.cpp
+++ libfoundation/src/system-stream.cpp
@@ -1,19 +1,19 @@
 /*                                                                     -*-c++-*-
-Copyright (C) 2015 LiveCode Ltd.
+   Copyright (C) 2015 LiveCode Ltd.
 
-This file is part of LiveCode.
+   This file is part of LiveCode.
 
-LiveCode is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License v3 as published by the Free
-Software Foundation.
+   LiveCode is free software; you can redistribute it and/or modify it under
+   the terms of the GNU General Public License v3 as published by the Free
+   Software Foundation.
 
-LiveCode is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
+   LiveCode is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+   for more details.
 
-You should have received a copy of the GNU General Public License
-along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
+   You should have received a copy of the GNU General Public License
+   along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
 
 /* Force the use of 64-bit file streams API on Linux.  We need to
  * define this before including foundation.h because it pulls in
@@ -82,17 +82,17 @@ static bool
 __MCSStreamInitializeErrors (void)
 {
 	if (!MCNamedErrorTypeInfoCreate(
-	        MCNAME("com.livecode.stream.IOError"),
-			MCNAME("stdio"),
-	        MCSTR("Stream input/output error: %{description}"),
-	        kMCSStreamIOErrorTypeInfo))
+		    MCNAME("com.livecode.stream.IOError"),
+		    MCNAME("stdio"),
+		    MCSTR("Stream input/output error: %{description}"),
+		    kMCSStreamIOErrorTypeInfo))
 		return false;
 
 	if (!MCNamedErrorTypeInfoCreate(
-	         MCNAME("com.livecode.stream.EndOfFileError"),
-			 MCNAME("stdio"),
-	         MCSTR("Reached end of file while reading from stream"),
-	         kMCSStreamEndOfFileErrorTypeInfo))
+		    MCNAME("com.livecode.stream.EndOfFileError"),
+		    MCNAME("stdio"),
+		    MCSTR("Reached end of file while reading from stream"),
+		    kMCSStreamEndOfFileErrorTypeInfo))
 		return false;
 
 	return true;
@@ -362,17 +362,17 @@ __MCSStdioStreamTell (MCStreamRef p_stream,
 }
 
 MCStreamCallbacks __kMCSStdioStreamCallbacks = {
-    __MCSStdioStreamDestroy,
-    __MCSStdioStreamIsFinished,
-    nil,
-    __MCSStdioStreamRead,
-    nil,
-    __MCSStdioStreamWrite,
-    nil,
-    nil,
-    nil,
-    __MCSStdioStreamTell,
-    __MCSStdioStreamSeek,
+	__MCSStdioStreamDestroy,
+	__MCSStdioStreamIsFinished,
+	nil,
+	__MCSStdioStreamRead,
+	nil,
+	__MCSStdioStreamWrite,
+	nil,
+	nil,
+	nil,
+	__MCSStdioStreamTell,
+	__MCSStdioStreamSeek,
 };
 
 /* ================================================================
@@ -465,7 +465,7 @@ __MCSStreamInitialize (void)
 	if (!__MCSStreamInitializeErrors())
 		return false;
 
-    return true;
+	return true;
 }
 
 void
