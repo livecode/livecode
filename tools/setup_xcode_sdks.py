6319fe804169cb498764536865adceb20e8714df
Monte Goulding <monte@appisle.net>
2016-09-29 20:07:28 +1000

    Update setup xcode script for 10.9 SDK

--- tools/setup_xcode_sdks.py
+++ tools/setup_xcode_sdks.py
@@ -4,7 +4,7 @@
 
 iphoneos_versions = ["10.0", "9.2"]
 iphonesimulator_versions = ["10.0", "9.2", "8.2", "7.1", "6.1"]
-macosx_versions = ["10.8", "10.6"]
+macosx_versions = ["10.9", "10.6"]
 
 # This tool creates the symlinks required for Xcode builds of LiveCode.
 #
