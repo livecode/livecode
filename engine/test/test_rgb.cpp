36fd93b0b6735966daf7af0de266eaca096bf7dd
livecodeali <ali.lloyd@livecode.com>
2016-10-28 12:20:11 +0100

    [[ Bug ]] Use custom function for string compare in rgb test

--- engine/test/test_rgb.cpp
+++ engine/test/test_rgb.cpp
@@ -20,6 +20,38 @@ along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
 #include "util.h"
 #include "rgb.cpp"
 
+static char to_lower(char p_char)
+{
+	if ('A' <= p_char && p_char <= 'Z')
+		p_char += 32;
+	
+	return p_char;
+}
+
+// We assume color name strings are ASCII
+static compare_t string_caseless_compare(const char *left, const char *right)
+{
+	uindex_t t_left_length, t_right_length;
+	t_left_length = strlen(left);
+	t_right_length = strlen(right);
+	
+	for(;;)
+	{
+		if (t_left_length == 0 || t_right_length == 0)
+			break;
+
+		index_t t_diff;
+		t_diff = to_lower(*left++) - tolower(*right++);
+		if (t_diff != 0)
+			return t_diff;
+		
+		t_left_length -= 1;
+		t_right_length -= 1;
+	}
+	
+	return t_left_length - t_right_length;
+}
+
 TEST(rgb, color_table)
 //
 // Checks that the entries of rgb color table are in alphabetical order.
@@ -31,7 +63,8 @@ TEST(rgb, color_table)
 	ASSERT_GE(color_table_size, (unsigned)1);
 
 	for(uint4 i = 0; i < color_table_size - 1; i++) {
-		EXPECT_LT(MCU_strcasecmp(color_table[i].token, color_table[i+1].token), 0)
+		EXPECT_LT(string_caseless_compare(color_table[i].token,
+		                                  color_table[i+1].token), 0)
 			<< "\"" << color_table[i+1].token << "\""
 			<< " comes before "
 			<< "\"" << color_table[i].token << "\"";
