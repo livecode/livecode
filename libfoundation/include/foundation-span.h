eb68efa5afab5881eaddf7d593d55ba4436a0797
Peter TB Brett <peter.brett@livecode.com>
2017-03-27 11:53:25 +0100

    libfoundation: Add span-based versions of object hashes
    `MCHashSpan()` and `MCHashSpanStream()` are intended to assist with
    hashing a contiguous array of identical objects, e.g. hashing an array
    of `int`.

--- libfoundation/include/foundation-span.h
+++ libfoundation/include/foundation-span.h
@@ -214,4 +214,24 @@ hash_t MCHashNativeChars(MCSpan<const char_t> chars);
 MC_DLLEXPORT
 hash_t MCHashChars(MCSpan<const unichar_t> chars);
 
+template <typename ElementType>
+inline hash_t MCHashSpan(MCSpan<ElementType> p_span)
+{
+    /* TODO[C++11] Enable this assertion once all our C++ compilers support it. */
+    /* static_assert(std::is_trivially_copyable<ElementType>::value,
+           "MCHashObjectSpan can only be used with trivially copyable types"); */
+    return MCHashBytes(p_span.data(), p_span.sizeBytes());
+}
+
+template <typename ElementType>
+inline hash_t MCHashSpanStream(hash_t p_previous,
+                               MCSpan<ElementType> p_span)
+{
+    /* TODO[C++11] Enable this assertion once all our C++ compilers support it. */
+    /* static_assert(std::is_trivially_copyable<ElementType>::value,
+           "MCHashObjectSpanStream can only be used with trivially copyable types"); */
+    return MCHashBytesStream(p_previous,
+                             p_span.data(), p_span.sizeBytes());
+}
+
 #endif /* !__MC_FOUNDATION_SPAN_H__ */
