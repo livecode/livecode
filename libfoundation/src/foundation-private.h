1749e164658cb56e8e4973da50b6e98338bd9a05
Peter TB Brett <peter.brett@livecode.com>
2017-03-31 22:57:52 +0100

    [Bug 14861] libfoundation: Ensure private header includes <utility>
    The newly-introduced `MCInplaceReverse()` function requires
    `<utility>` to get `std::swap()`.

--- libfoundation/src/foundation-private.h
+++ libfoundation/src/foundation-private.h
@@ -19,7 +19,7 @@ along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
 
 
 #include <stdio.h>
-
+#include <utility>
 
 ////////////////////////////////////////////////////////////////////////////////
 
