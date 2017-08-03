a46449041e752935150276611bd6958c7c9f9dbc
Ian Macphail <ian.macphail@livecode.com>
2017-08-03 15:30:41 +0100

    [[ Prebuilts ]] Fix incorrect archive filename

--- prebuilt/archive-libs.sh
+++ prebuilt/archive-libs.sh
@@ -8,14 +8,15 @@ ARCH=$2
 SUBPLATFORM=$3
 
 if [ -z "${SUBPLATFORM}" ] ; then
-	ARCHIVE_TAR="../${ARCH}-${PLATFORM}-prebuilts.tar"
+	ARCHIVE_TAR="${ARCH}-${PLATFORM}-prebuilts.tar"
 else
-	ARCHIVE_TAR="../${ARCH}-${PLATFORM}-${SUBPLATFORM}.tar"
+	ARCHIVE_TAR="${ARCH}-${PLATFORM}-${SUBPLATFORM}-prebuilts.tar"
 fi
 
+echo "Creating archive ${ARCHIVE_TAR}"
 # Add packages to archive
-tar -cf "${ARCHIVE_TAR}" "${PACKAGE_DIR}/"
+tar -cf "../${ARCHIVE_TAR}" "${PACKAGE_DIR}/"
 
-if [ -f "${ARCHIVE_TAR}" ] ; then
-	bzip2 -zf --best "${ARCHIVE_TAR}"
+if [ -f "../${ARCHIVE_TAR}" ] ; then
+	bzip2 -zf --best "../${ARCHIVE_TAR}"
 fi
