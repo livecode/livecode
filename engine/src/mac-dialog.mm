6d413f8f3f239af2adce2ef1d738152539d250b3
Brian Milby <brian@milby7.com>
2017-09-05 20:42:07 -0500

    Restore "new folder" button on Mac folder dialog
    When the file/folder routine was abstracted (commit 93b3d28), the "new folder" button was removed from the folder dialog.  This commit restores the code line and original comment where it would have landed in the updated function.  I originally branched this off of develop due to the missing save dialog, but due to this regression it should probably be applied to 8.1.7 as well.

--- engine/src/mac-dialog.mm
+++ engine/src/mac-dialog.mm
@@ -629,6 +629,9 @@ void MCPlatformBeginFolderOrFileDialog(MCPlatformFileDialogKind p_kind, MCPlatfo
         [(NSOpenPanel *)t_panel setCanChooseFiles: NO];
         [(NSOpenPanel *)t_panel setCanChooseDirectories: YES];
         [(NSOpenPanel *)t_panel setAllowsMultipleSelection: NO];
+
+        // MM-2012-03-01: [[ BUG 10046]] Make sure the "new folder" button is enabled for folder dialogs
+        [t_panel setCanCreateDirectories: YES];
     }
     
     if (p_kind != kMCPlatformFileDialogKindSave && 
