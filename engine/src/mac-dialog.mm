livecode/livecode

commit 93b3d28515dbdba24c85b8f502406e5e65aed459

Abstracted the routine called for answer file/folder

engine/src/mac-dialog.mm
@@ -204,48 +204,6 @@ static MCPlatformDialogResult MCPlatformEndOpenSaveDialog(void)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void MCPlatformBeginFolderDialog(MCPlatformWindowRef p_owner, MCStringRef p_title, MCStringRef p_prompt, MCStringRef p_initial)
-{
-    MCAutoStringRef t_initial_folder;
-    
-    if (p_initial != nil)
-    /* UNCHECKED */ folder_path_from_initial_path(p_initial, &t_initial_folder);
-    
-    NSOpenPanel *t_panel;
-    t_panel = [NSOpenPanel openPanel];
-    // If we have both a title and a message, then 'title' is used on setTitle, and 'prompt' is used for message.
-    // If there is no title, then on 10.11+ we must use the message field of the dialog; on <10.11 we must use the title field
-    if (p_title != nil && MCStringGetLength(p_title) != 0)
-    {
-        [t_panel setTitle: [NSString stringWithMCStringRef: p_title]];
-        [t_panel setMessage: [NSString stringWithMCStringRef: p_prompt]];
-    }
-    else
-    {
-        extern uint4 MCmajorosversion;
-        if (MCmajorosversion >= 0x10B0)
-            [t_panel setMessage: [NSString stringWithMCStringRef: p_prompt]];
-        else
-            [t_panel setTitle: [NSString stringWithMCStringRef: p_prompt]];
-        
-    }
-    [t_panel setPrompt: @"Choose"];
-    [t_panel setCanChooseFiles: NO];
-    [t_panel setCanChooseDirectories: YES];
-    [t_panel setAllowsMultipleSelection: NO];
-    
-    // MM-2012-03-01: [[ BUG 10046]] Make sure the "new folder" button is enabled for folder dialogs
-    [t_panel setCanCreateDirectories: YES];
-    
-    if ([t_panel respondsToSelector:@selector(isAccessoryViewDisclosed)])
-    {
-        // show accessory view when dialog opens
-        [t_panel setAccessoryViewDisclosed: YES];
-    }
-	
-	MCMacPlatformBeginOpenSaveDialog(p_owner, t_panel, *t_initial_folder, nil);
-}
-
 MCPlatformDialogResult MCPlatformEndFolderDialog(MCStringRef& r_selected_folder)
 {	
 	if (s_dialog_nesting -> result == kMCPlatformDialogResultContinue)
@@ -605,70 +563,82 @@ static bool hfs_code_to_string(unsigned long p_code, char *r_string)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void MCPlatformBeginFileDialog(MCPlatformFileDialogKind p_kind, MCPlatformWindowRef p_owner, MCStringRef p_title, MCStringRef p_prompt, MCStringRef *p_types, uint4 p_type_count, MCStringRef p_initial)
+void MCPlatformBeginFolderOrFileDialog(MCPlatformFileDialogKind p_kind, MCPlatformWindowRef p_owner, MCStringRef p_title, MCStringRef p_prompt, MCStringRef *p_types, uint4 p_type_count, MCStringRef p_initial, bool p_is_file)
 {
-	MCAutoStringRef t_initial_folder;
-	if (p_initial != nil)
-		/* UNCHECKED */ folder_path_from_initial_path(p_initial, &t_initial_folder);
-	
-	MCAutoStringRef t_initial_file;
-	if ((p_kind == kMCPlatformFileDialogKindSave) && p_initial != nil && !MCS_exists(p_initial, false))
-	{
-		uindex_t t_last_slash;
+    MCAutoStringRef t_initial_folder;
+    if (p_initial != nil)
+    /* UNCHECKED */ folder_path_from_initial_path(p_initial, &t_initial_folder);
+    
+    MCAutoStringRef t_initial_file;
+    if ((p_kind == kMCPlatformFileDialogKindSave) && p_initial != nil && !MCS_exists(p_initial, false))
+    {
+        uindex_t t_last_slash;
         if (MCStringLastIndexOfChar(p_initial, '/', UINT32_MAX, kMCStringOptionCompareExact, t_last_slash))
             // SN-2014-08-11: [[ Bug 13143 ]] Take the right part: after the last slash, not before
             MCStringCopySubstring(p_initial, MCRangeMake(t_last_slash + 1, MCStringGetLength(p_initial) - t_last_slash - 1), &t_initial_file);
         else
             t_initial_file = p_initial;
-	}
-	
-	NSSavePanel *t_panel;
-	t_panel = (p_kind == kMCPlatformFileDialogKindSave) ? [NSSavePanel savePanel] : [NSOpenPanel openPanel] ;
-	
-	if (p_title != nil && !MCStringIsEmpty(p_title))
-	{
-		[t_panel setTitle: [NSString stringWithMCStringRef: p_title]];
-		[t_panel setMessage: [NSString stringWithMCStringRef: p_prompt]];
-	}
-	else
-	{
-		extern uint4 MCmajorosversion;
-		if (MCmajorosversion >= 0x10B0 && p_kind != kMCPlatformFileDialogKindSave)
-			[t_panel setMessage: [NSString stringWithMCStringRef: p_prompt]];
-		else
-			[t_panel setTitle: [NSString stringWithMCStringRef: p_prompt]];
-	}
-	
-    // MW-2014-07-17: [[ Bug 12826 ]] If we have at least one type, add a delegate. Only add as
-    //   an accessory view if more than one type.
-    MCFileDialogAccessoryView *t_accessory;
-    if (p_type_count > 0)
-    {
-        t_accessory = [[MCFileDialogAccessoryView alloc] initWithPanel: t_panel];
-        [t_accessory setTypes: p_types length: p_type_count];
-        [t_accessory setLabel: @"Format:"];
-        if (p_type_count > 1)
-            [t_panel setAccessoryView: t_accessory];
-        [t_panel setDelegate: t_accessory];
-	}
+    }
     
-	if (p_kind != kMCPlatformFileDialogKindSave)
-	{
-		[(NSOpenPanel *)t_panel setCanChooseFiles: YES];
-		[(NSOpenPanel *)t_panel setCanChooseDirectories: NO];
-		[(NSOpenPanel *)t_panel setAllowsMultipleSelection: p_kind == kMCPlatformFileDialogKindOpenMultiple ? YES : NO];
-	}
-	// MM-2012-03-01: [[ BUG 10046]] Make sure the "new folder" button is enabled for save dialogs
-	else 
-		[t_panel setCanCreateDirectories: YES];
-	
-	if ([t_panel respondsToSelector:@selector(isAccessoryViewDisclosed)])
-	{
-		// show accessory view when dialog opens
-		[t_panel setAccessoryViewDisclosed: YES];
-	}
-	
-	MCMacPlatformBeginOpenSaveDialog(p_owner, t_panel, *t_initial_folder, *t_initial_file);
+    NSSavePanel *t_panel;
+    t_panel = (p_kind == kMCPlatformFileDialogKindSave) ? [NSSavePanel savePanel] : [NSOpenPanel openPanel] ;
+    
+    // If we have both a title and a message, then 'title' is used on setTitle, and 'prompt' is used for message.
+    // If there is no title, then on 10.11+ we must use the message field of the dialog; on <10.11 we must use the title field
+    if (p_title != nil && !MCStringIsEmpty(p_title))
+    {
+        [t_panel setTitle: [NSString stringWithMCStringRef: p_title]];
+        [t_panel setMessage: [NSString stringWithMCStringRef: p_prompt]];
+    }
+    else
+    {
+        extern uint4 MCmajorosversion;
+        if (MCmajorosversion >= 0x10B0 && p_kind != kMCPlatformFileDialogKindSave)
+            [t_panel setMessage: [NSString stringWithMCStringRef: p_prompt]];
+        else
+            [t_panel setTitle: [NSString stringWithMCStringRef: p_prompt]];
+    }
+    
+    if (p_is_file)
+    {
+        // MW-2014-07-17: [[ Bug 12826 ]] If we have at least one type, add a delegate. Only add as
+        //   an accessory view if more than one type.
+        MCFileDialogAccessoryView *t_accessory;
+        if (p_type_count > 0)
+        {
+            t_accessory = [[MCFileDialogAccessoryView alloc] initWithPanel: t_panel];
+            [t_accessory setTypes: p_types length: p_type_count];
+            [t_accessory setLabel: @"Format:"];
+            if (p_type_count > 1)
+                [t_panel setAccessoryView: t_accessory];
+            [t_panel setDelegate: t_accessory];
+        }
+        
+        if (p_kind != kMCPlatformFileDialogKindSave)
+        {
+            [(NSOpenPanel *)t_panel setCanChooseFiles: YES];
+            [(NSOpenPanel *)t_panel setCanChooseDirectories: NO];
+            [(NSOpenPanel *)t_panel setAllowsMultipleSelection: p_kind == kMCPlatformFileDialogKindOpenMultiple ? YES : NO];
+        }
+        // MM-2012-03-01: [[ BUG 10046]] Make sure the "new folder" button is enabled for save dialogs
+        else
+            [t_panel setCanCreateDirectories: YES];
+    }
+    else
+    {
+        [t_panel setPrompt: @"Choose"];
+        [t_panel setCanChooseFiles: NO];
+        [t_panel setCanChooseDirectories: YES];
+        [t_panel setAllowsMultipleSelection: NO];
+    }
+    
+    if ([t_panel respondsToSelector:@selector(isAccessoryViewDisclosed)])
+    {
+        // show accessory view when dialog opens
+        [t_panel setAccessoryViewDisclosed: YES];
+    }
+    
+    MCMacPlatformBeginOpenSaveDialog(p_owner, t_panel, *t_initial_folder, p_is_file ? *t_initial_file : nil);
 }
 
 MCPlatformDialogResult MCPlatformEndFileDialog(MCPlatformFileDialogKind p_kind, MCStringRef &r_paths, MCStringRef &r_type)

engine/src/desktop-ans.cpp
@@ -60,7 +60,7 @@ int MCA_folder(MCStringRef p_title, MCStringRef p_prompt, MCStringRef p_initial,
 	MCPlatformWindowRef t_owner;
     t_owner = compute_sheet_owner(p_options);
 	
-	MCPlatformBeginFolderDialog(t_owner, p_title, p_prompt, p_initial);
+	MCPlatformBeginFolderOrFileDialog(kMCPlatformFileDialogKindFolder, t_owner, p_title, p_prompt, nil, 1, p_initial, false);
 	
 	MCPlatformDialogResult t_result;
 	MCAutoStringRef t_folder;
@@ -158,7 +158,7 @@ int MCA_file(MCStringRef p_title, MCStringRef p_prompt, MCStringRef p_filter, MC
 	else
 		t_kind = kMCPlatformFileDialogKindOpen;
 	
-	MCPlatformBeginFileDialog(t_kind, t_owner, p_title, p_prompt, *t_types, t_types . Count(), p_initial);
+	MCPlatformBeginFolderOrFileDialog(t_kind, t_owner, p_title, p_prompt, *t_types, t_types . Count(), p_initial, true);
 	
 	MCPlatformDialogResult t_result;
 	MCAutoStringRef t_file, t_type;
@@ -191,7 +191,7 @@ int MCA_file_with_types(MCStringRef p_title, MCStringRef p_prompt, MCStringRef *
 	else
 		t_kind = kMCPlatformFileDialogKindOpen;
 	
-	MCPlatformBeginFileDialog(t_kind, t_owner, p_title, p_prompt, p_types, p_type_count, p_initial);
+	MCPlatformBeginFolderOrFileDialog(t_kind, t_owner, p_title, p_prompt, p_types, p_type_count, p_initial, true);
 	
 	MCPlatformDialogResult t_result;
 	MCAutoStringRef t_file, t_type;
@@ -227,7 +227,7 @@ int MCA_ask_file(MCStringRef p_title, MCStringRef p_prompt, MCStringRef p_filter
 	MCPlatformWindowRef t_owner;
     t_owner = compute_sheet_owner(p_options);
 	
-	MCPlatformBeginFileDialog(kMCPlatformFileDialogKindSave, t_owner, p_title, p_prompt, *t_types, t_types . Count(), p_initial);
+	MCPlatformBeginFolderOrFileDialog(kMCPlatformFileDialogKindSave, t_owner, p_title, p_prompt, *t_types, t_types . Count(), p_initial, true);
 	
 	MCPlatformDialogResult t_result;
 	MCAutoStringRef t_file, t_type;
@@ -254,7 +254,7 @@ int MCA_ask_file_with_types(MCStringRef p_title, MCStringRef p_prompt, MCStringR
 	MCPlatformWindowRef t_owner;
     t_owner = compute_sheet_owner(p_options);
 	
-	MCPlatformBeginFileDialog(kMCPlatformFileDialogKindSave, t_owner, p_title, p_prompt, p_types, p_type_count, p_initial);
+	MCPlatformBeginFolderOrFileDialog(kMCPlatformFileDialogKindSave, t_owner, p_title, p_prompt, p_types, p_type_count, p_initial, true);
 	
 	MCPlatformDialogResult t_result;
 	MCAutoStringRef t_file, t_type;

engine/src/platform.h
@@ -939,13 +939,13 @@ enum MCPlatformFileDialogKind
 	kMCPlatformFileDialogKindSave,
 	kMCPlatformFileDialogKindOpen,
 	kMCPlatformFileDialogKindOpenMultiple,
+    kMCPlatformFileDialogKindFolder,
 };
 
-void MCPlatformBeginFolderDialog(MCPlatformWindowRef owner, MCStringRef p_title, MCStringRef p_message, MCStringRef p_initial);
+void MCPlatformBeginFolderOrFileDialog(MCPlatformFileDialogKind p_kind, MCPlatformWindowRef p_owner, MCStringRef p_title, MCStringRef p_prompt, MCStringRef *p_types, uint4 p_type_count, MCStringRef p_initial, bool p_is_file);
 MCPlatformDialogResult MCPlatformEndFolderDialog(MCStringRef & r_selected_folder);
 
 
-void MCPlatformBeginFileDialog(MCPlatformFileDialogKind p_kind, MCPlatformWindowRef p_owner, MCStringRef p_title, MCStringRef p_prompt,  MCStringRef *p_types, uint4 p_type_count, MCStringRef p_initial);
 MCPlatformDialogResult MCPlatformEndFileDialog(MCPlatformFileDialogKind p_kind, MCStringRef& r_paths, MCStringRef& r_type);
 
 void MCPlatformBeginColorDialog(MCStringRef p_title, const MCColor& p_color);

commit f5ccb8177808b343f02b9f142b1dace15abfcc08

[[Bug 20335]] Mac folder dialog missing "add folder" button
Commit 93b3d28 introduced a regression where the "add folder" button was removed from the folder dialog.
This issue was originally fixed 3/1/12 for bug 10046.

engine/src/mac-dialog.mm
@@ -630,6 +630,9 @@ void MCPlatformBeginFolderOrFileDialog(MCPlatformFileDialogKind p_kind, MCPlatfo
         [t_panel setCanChooseFiles: NO];
         [t_panel setCanChooseDirectories: YES];
         [t_panel setAllowsMultipleSelection: NO];
+
+        // MM-2012-03-01: [[ BUG 10046]] Make sure the "new folder" button is enabled for folder dialogs
+        [t_panel setCanCreateDirectories: YES];
     }
     
     if ([t_panel respondsToSelector:@selector(isAccessoryViewDisclosed)])