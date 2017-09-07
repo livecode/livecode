9d15d4abab4f084dfb0fa914db33673ab6fcaeaf
Monte Goulding <monte@appisle.net>
2017-09-07 13:21:35 +1000

    [[ Builder ]] Generate more useful sha1sum file

--- buildbot.mk
+++ buildbot.mk
@@ -224,7 +224,7 @@ dist-upload-files.txt sha1sum.txt:
 		find . -maxdepth 1 -name 'LiveCodeNotes*.html' >> dist-upload-file; \
 	fi; \
 	if test "$(UPLOAD_ENABLE_CHECKSUM)" = "yes"; then \
-	  $(SHA1SUM) < dist-upload-files.txt > sha1sum.txt; \
+	  xargs --arg-file=dist-upload-files.txt $(SHA1SUM) > sha1sum.txt; \
 	  echo sha1sum.txt >> dist-upload-files.txt; \
 	else \
 	  touch sha1sum.txt; \
