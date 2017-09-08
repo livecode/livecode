6b7c86762eac7389373ada0d770aca31414b1c73
Ian Macphail <ian@livecode.com>
2017-09-08 16:36:05 +0100

    [[ ICUData ]] Fix 'crlf' -> 'lf' in encoded binary files

--- util/encode_data.pl
+++ util/encode_data.pl
@@ -10,9 +10,9 @@ my $varName		= $ARGV[2];
 # Read in the source file
 open INPUT, "<$sourceFile"
 	or die "Could not open source file \"$sourceFile\": $!";
-my @lines = <INPUT>;
+binmode INPUT;
+my $input = do { local( $/ ) ; <INPUT> } ;
 close INPUT;
-my $input = join('', @lines); 
 	
 # Split into bytes
 my @bytes = unpack('C*', $input);
