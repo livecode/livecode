850f46c1d0ffe18f367af37aac0a9d090e7d35aa
Fraser J. Gordon <fraser.gordon@xvi.io>
2017-10-16 23:56:34 -0400

    Add support for running tests under an interpreter
    This is needed when running the test-suite for a cross-compiled engine: the
    interpreter can be set to an emulator in order to execute the test suite
    using the target executables.

--- util/run-tests.pl
+++ util/run-tests.pl
@@ -7,6 +7,11 @@ $test_exec = $ARGV[1];
 $test_dir = dirname($test_exec);
 $test_log = $ARGV[2];
 
+# Use an interpreter, if specified.
+if ($test_wrapper eq "" && $ENV{"INTERPRETER"} ne "") {
+	$test_wrapper = $ENV{"INTERPRETER"};
+}
+
 $test_command_line = "$test_wrapper $test_exec --tap=$test_log";
 
 chdir $test_dir or die "Failed to change directory!";
