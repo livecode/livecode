1cd97052156fd87c50141d000229d1151a4eed91
livecodeali <ali.lloyd@gmail.com>
2018-02-15 16:54:53 +0000

    [[ TimeZoneLibrary ]] Add tz lib source

---
+++ extensions/libraries/timezone/tz/workman.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+# This file is in the public domain, so clarified as of
+# 2009-05-17 by Arthur David Olson.
+
+# Tell groff not to emit SGR escape sequences (ANSI color escapes).
+GROFF_NO_SGR=1
+export GROFF_NO_SGR
+
+echo ".am TH
+.hy 0
+.na
+..
+.rm }H
+.rm }F" | nroff -man - ${1+"$@"} | perl -ne '
+	binmode STDIN, '\'':encoding(utf8)'\'';
+	binmode STDOUT, '\'':encoding(utf8)'\'';
+	chomp;
+	s/.\010//g;
+	s/\s*$//;
+	if (/^$/) {
+		$sawblank = 1;
+		next;
+	} else {
+		if ($sawblank && $didprint) {
+			print "\n";
+			$sawblank = 0;
+		}
+		print "$_\n";
+		$didprint = 1;
+	}
+'
