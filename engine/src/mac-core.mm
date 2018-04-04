51a08a6e79e4b965ecd888d7a4d2f0c29851a1c7
Monte Goulding <monte@appisle.net>
2018-04-04 13:21:58 +1000

    [[ Cleanup ]] Cast app delegate
    This patch casts the app delegate to ensure the compiler treats the return
    value as the correct type `OSErr` rather than defaulting to `id`.

--- engine/src/mac-core.mm
+++ engine/src/mac-core.mm
@@ -233,7 +233,7 @@ enum
 
 static OSErr preDispatchAppleEvent(const AppleEvent *p_event, AppleEvent *p_reply, SRefCon p_context)
 {
-    return [[NSApp delegate] preDispatchAppleEvent: p_event withReply: p_reply];
+    return [(com_runrev_livecode_MCApplicationDelegate*)[NSApp delegate] preDispatchAppleEvent: p_event withReply: p_reply];
 }
 
 - (OSErr)preDispatchAppleEvent: (const AppleEvent *)p_event withReply: (AppleEvent *)p_reply
