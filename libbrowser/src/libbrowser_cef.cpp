67615062271488390d4537e0ab5b7ceb080356e7
Monte Goulding <monte@appisle.net>
2018-09-05 12:49:15 +1000

    [[ CEF ]] Update CEF to 3.3440.1806.g65046b7

--- libbrowser/src/libbrowser_cef.cpp
+++ libbrowser/src/libbrowser_cef.cpp
@@ -985,7 +985,7 @@ public:
 	// CefRequestHandler interface
 	
 	// Called on UI thread
-	virtual bool OnBeforeBrowse(CefRefPtr<CefBrowser> p_browser, CefRefPtr<CefFrame> p_frame, CefRefPtr<CefRequest> p_request, bool p_is_redirect) OVERRIDE
+	virtual bool OnBeforeBrowse(CefRefPtr<CefBrowser> p_browser, CefRefPtr<CefFrame> p_frame, CefRefPtr<CefRequest> p_request, bool p_user_gesture, bool p_is_redirect) OVERRIDE
 	{
 		// IM-2014-07-21: [[ Bug 12296 ]] If browser has been closed then exit
 		if (nil == m_owner)
