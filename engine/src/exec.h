9bbc1383363af4464aefc9e0458336bae3af2658
Mark Waddingham <mark.waddingham@livecode.com>
2018-11-07 11:15:05 +0000

    switchopt

--- engine/src/exec.h
+++ engine/src/exec.h
@@ -1786,7 +1786,8 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void MCKeywordsExecSwitch(MCExecContext& ctxt, MCExpression *condition, MCExpression **cases, uindex_t case_count, int2 default_case, uint2 *case_offsets, MCStatement *statements, uint2 line, uint2 pos);
+void MCKeywordsExecSwitch(MCExecContext& ctxt, MCExpression *condition, MCExpression **cases, uindex_t case_count, MCStatement* defaultstmt, MCStatement **casestmts, uint2 line, uint2 pos);
+void MCKeywordsExecStaticSwitch(MCExecContext& ctxt, MCExpression *condition, MCArrayRef cases, MCStatement* defaultstmt, MCStatement **casestmts, uint2 line, uint2 pos);
 void MCKeywordsExecIf(MCExecContext& ctxt, MCExpression *condition, MCStatement *thenstatements, MCStatement *elsestatements, uint2 line, uint2 pos);
 void MCKeywordsExecRepeatCount(MCExecContext& ctxt, MCStatement *statements, MCExpression *endcond, uint2 line, uint2 pos);
 void MCKeywordsExecRepeatFor(MCExecContext& ctxt, MCStatement *statements, MCExpression *endcond, MCVarref *loopvar, File_unit each, uint2 line, uint2 pos);
