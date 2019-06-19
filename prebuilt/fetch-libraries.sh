200323af5c1852a53605c7a157f646a0e202e14a
Ali Lloyd <ali.lloyd@gmail.com>
2019-06-19 08:34:45 +0100

    [[ Android API 28 ]] Update to use android API 28
    This patch changes our build scripts to use or default to android
    api 28. It also removes the builders' dependence on the sdk API
    version so we can use the same build containers for any sdk API and
    only have to update (and rebuild prebuilts) when the ndk changes.

--- prebuilt/fetch-libraries.sh
+++ prebuilt/fetch-libraries.sh
@@ -17,7 +17,7 @@ LIBS_emscripten=( Thirdparty ICU )
 
 SUBPLATFORMS_ios=(iPhoneSimulator8.2 iPhoneSimulator9.2 iPhoneSimulator10.2 iPhoneSimulator11.2 iPhoneSimulator12.1 iPhoneOS9.2 iPhoneOS10.2 iPhoneOS11.2 iPhoneOS12.1)
 SUBPLATFORMS_win32=(v141_static_debug v141_static_release)
-SUBPLATFORMS_android=(sdk26_ndk16r15)
+SUBPLATFORMS_android=(ndk16r15)
 
 # Fetch settings
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
