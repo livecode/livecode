1f720fbc6788e72da66ff6229cf7581913fc575b
Mark Waddingham <mark.waddingham@livecode.com>
2019-09-19 17:05:54 +0100

    [[ Bug 22760 ]] Fix 'external' handler invocation
    This patch ensures that when a handler-ref is invoked from the main/ui thread
    on mobile that a jump is made to the script/engine thread before being
    executed.
    
    After the handler is executed the JNI environment is reset for the calling
    thread. This patch also fixes an issue where `MCJavaGetJObjectClassName` did
    not call `MCJavaDoAttachCurrentThread` and therefore could access JNI
    environment attached to a different thread.
    
    (cherry picked from commit 15eff1554567cc63e5d6509b29201dc89d8ff20b)

--- libfoundation/src/foundation-java-private.cpp
+++ libfoundation/src/foundation-java-private.cpp
@@ -555,7 +555,9 @@ bool MCJavaObjectCreateNullable(jobject p_object, MCJavaObjectRef &r_object)
 
 static jstring MCJavaGetJObjectClassName(jobject p_obj)
 {
-    /* Make sure the fetched class local-refs are deleted on exit */
+	MCJavaDoAttachCurrentThread();
+	
+	/* Make sure the fetched class local-refs are deleted on exit */
     MCJavaAutoLocalRef<jclass> t_class =
         s_env->GetObjectClass(p_obj);
     MCJavaAutoLocalRef<jclass> javaClassClass =
@@ -1955,10 +1957,17 @@ jobject MCJavaPrivateDoNativeListenerCallback(jlong p_handler, jstring p_method_
     MCProperListRef t_mutable_list;
     if (!MCProperListMutableCopy(*t_list, t_mutable_list))
         return nullptr;
-    
-    MCErrorRef t_error =
+	
+	/* The handler is run on the script thread and could call code which executes
+	 * MCJavaDoAttachCurrentThread() so we must ensure s_env is reset correctly for the
+	 * current thread */
+	JNIEnv *t_old_env;
+	t_old_env = s_env;
+	MCErrorRef t_error =
         MCHandlerTryToExternalInvokeWithList(static_cast<MCHandlerRef>(t_handler),
                                              t_mutable_list, &t_result);
+	s_env = t_old_env;
+	
     jobject t_return = nullptr;
     if (*t_result != nil)
     {
